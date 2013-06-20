19555ec49adb8fbd19872ba28541b9767cb9d919
Tibor Katelbach <oceatoon@gmail.com>
2013-06-20 11:48:00 +0400

    clean up

--- config/configDB.php
+++ config/configDB.php
@@ -1,7 +1,6 @@
 <?php
 $dbconfig = array(
 	'connectionString' => 'mongodb://mongoLabUser:mongoLabPwd@mongoLabServer.mongolab.com:mongoLabServer/pixelhumain',
-
-	'username' => 'pixelhumaindb',
-	'password' => '2210ph');
+	'username' => '',
+	'password' => '');
 ?>
