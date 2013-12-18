841dfc42bcddf9863b1c3b3e2fd6b90851dfd98f
Tibor Katelbach <oceatoon@gmail.com>
2013-12-18 16:14:04 +0400

    url tests

--- ph/protected/controllers/ext/IrdCharcController.php
+++ ph/protected/controllers/ext/IrdCharcController.php
@@ -8,7 +8,7 @@
  * @author: Tibor Katelbach <tibor@pixelhumain.com>
  * Date: 15/08/13
  */
-class IrdCharcController extends Controller {
+class IrdcharcController extends Controller {
 
 	public function actionIndex() {
 	    echo "IRD Controller";
