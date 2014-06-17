7101bffa67a02a4b31de7cc3aa5b8ea92d828a95
Tibor Katelbach <oceatoon@gmail.com>
2014-06-17 12:02:03 +0400

    testing commit

---
+++ ph/protected/controllers/user/SendEmailPwdAction.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * [actionAddWatcher 
+ * create or update a user account
+ * if the email doesn't exist creates a new citizens with corresponding data 
+ * else simply adds the watcher app the users profile ]
+ * @return [json] 
+ */
+class SendEmailPwdAction extends CAction
+{
+    public function run()
+    {
+       $email = $_POST["email"];
+        if($user = PHDB::findOne(PHType::TYPE_CITOYEN,array( "email" => $email ) ))
+        {
+            //reset password 
+            $pwd = self::random_password(8);
+            PHDB::update( PHType::TYPE_CITOYEN,
+                            array("email" => $email), 
+                            array('$set' => array("pwd"=>hash('sha256', $email.$pwd))));
+
+            //send validation mail
+            //TODO : make emails as cron jobs
+            $message = new YiiMailMessage;
+            $message->view = 'passwordRetreive';
+            $message->setSubject('Votre Mot de passe Pixel Humain');
+            $message->setBody(array("pwd"=>$pwd), 'text/html');
+            if(!PH::notlocalServer())
+                $message->addTo(Yii::app()->params['adminEmail']);
+            else
+                $message->addTo($email);
+            $message->from = Yii::app()->params['adminEmail'];
+            Yii::app()->mail->send($message);
+
+            $res = array("result"=>true,"msg"=>"Veuillez vérifier votre boite Email, vous y trouverez votre mot de passe.");
+        } else {
+            //TODO evoyer un email de presentation 
+            $res = array("result"=>false,"msg"=>"Cet email n'existe pas, vous pouvez le creer par contre.");
+        }
+        Rest::json($res);  
+        Yii::app()->end();
+    }
+    public function random_password( $length = 8 ) {
+        $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-=+;:,.?";
+        $password = substr( str_shuffle( $chars ), 0, $length );
+        return $password;
+    }
+}
