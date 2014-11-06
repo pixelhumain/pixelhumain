573c34c08d4efc30fed3183ca17ed964011e9529
Luc Bonnin <lucbonnin@gmail.com>
2014-11-06 17:16:51 +0100

    fix previous bad commit with modulePath, sorry

--- ph/protected/config/main.php
+++ ph/protected/config/main.php
@@ -18,12 +18,12 @@ return array(
 
 	// preloading 'log' component
 	'preload'=>array('log'),
-	'modulePath' =>realpath(__DIR__.'../../modules'),
+	'modulePath' =>realpath(__DIR__.'/../../../modules'),
 	'aliases' => array(
     	'vendor' => realpath(__DIR__ . '/../../vendor/'),
         'bootstrap' => realpath(__DIR__ . '/../../vendor/2amigos/yiistrap'),
 		'yiiwheels' => realpath(__DIR__ . '/../../vendor/2amigos/yiiwheels'), 
-    	'mongoYii' => realpath(__DIR__ . '/../../vendor/sammaye/mongoyii')
+    	'mongoYii' => realpath(__DIR__ . '/../../vendor/sammaye/mongoyii'),		
     ),
     'controllerMap'=>array(
          //'YiiFeedWidget' => 'ext.yii-feed-widget.YiiFeedWidgetController'
@@ -104,6 +104,7 @@ return array(
                '<controller:\w+>/<id:\d+>' => '<controller>/view',
                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+			  // '<module:\w+>/<controller:\w+>/<action:\w+>'=>'<module>/<controller>/<action>',
 			),
 		),
 		/*'db' => array(
