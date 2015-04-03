6253caff393f08c56a0039b38025d3f674782480
Tibor Katelbach <oceatoon@gmail.com>
2015-04-03 20:25:15 +0400

    clear a case issue for the server

--- ph/protected/config/main.php
+++ ph/protected/config/main.php
@@ -25,7 +25,7 @@ return array(
 	'modulePath' => realpath(__DIR__ . '/../../../../modules/'),
 	'aliases' => array(
     	'vendor' => realpath(__DIR__ . '/../../vendor/'),
-    	'mongoYii' => realpath(__DIR__ . '/../../vendor/sammaye/mongoyii'),
+    	'mongoYii' => realpath(__DIR__ . '/../../vendor/sammaye/mongoYii'),
     	'bootstrap' => realpath(__DIR__ . '/../../vendor/2amigos/yiistrap'),
 		'yiiwheels' => realpath(__DIR__ . '/../../vendor/2amigos/yiiwheels'), 
     ),
