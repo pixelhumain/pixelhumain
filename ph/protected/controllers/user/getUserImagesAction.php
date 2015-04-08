c30fde91ed5e6714ba7e5e8a8c430d72dd61ea79
Robert Johnson <zeriam974@gmail.com>
2015-04-08 11:01:56 +0400

    get images files and new path for the upload

---
+++ ph/protected/controllers/user/getUserImagesAction.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * [actionAddWatcher 
+ * create or update a user account
+ * if the email doesn't exist creates a new citizens with corresponding data 
+ * else simply adds the watcher app the users profile ]
+ * @return [json] 
+ */
+class GetUserImagesAction extends CAction
+{
+    public function run($type, $id)
+    {
+    	$directory = 'upload/communecter/'.$type.'/'.$id.'/';
+		$listImages=array();
+		if(file_exists ( $directory )){
+	    	//get all image files with a .jpg extension. This way you can add extension parser
+	    	$images = glob($directory ."*.{jpg,png,gif}", GLOB_BRACE);
+	    	
+	    	foreach($images as $image){
+	        	array_push($listImages, Yii::app()->getRequest()->getBaseUrl(true)."/".$image);
+	    	}
+	    }
+	   	Rest::json($listImages);
+	    Yii::app()->end();
+    }
+}
