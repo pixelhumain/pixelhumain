11c6f8c2e4103fe429c1be885c93acb285b2e7f3
Robert Johnson <zeriam974@gmail.com>
2015-05-15 10:09:19 +0400

    Change upload Dir

--- ph/protected/config/paramsconfig.php
+++ ph/protected/config/paramsconfig.php
@@ -34,7 +34,7 @@ $params = array(
     'adminEmail'=>'contact@pixelhumain.com',
     //upload base directory
     'uploadURL' => "upload".DIRECTORY_SEPARATOR."communecter".DIRECTORY_SEPARATOR,
-    'uploadDir' => dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."upload".DIRECTORY_SEPARATOR."communecter".DIRECTORY_SEPARATOR,
+    'uploadDir' => dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."upload".DIRECTORY_SEPARATOR,
     'uploadComDir' => dirname(__FILE__).DIRECTORY_SEPARATOR.'..'."\\..\\templates\\upload\\dir\\communecter\\collection\\person",
     'captcha' => '6LdiygUTAAAAAEsbbK7LvMjJRt9PLP9lO-6QSM8K'
   );
