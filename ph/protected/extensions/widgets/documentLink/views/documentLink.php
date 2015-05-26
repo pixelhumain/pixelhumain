465f96c94633138c5285d767566beb96f473b934
Tibor Katelbach <oceatoon@gmail.com>
2015-05-26 18:25:35 +0400

    updating document Links

--- ph/protected/extensions/widgets/documentLink/views/documentLink.php
+++ ph/protected/extensions/widgets/documentLink/views/documentLink.php
@@ -3,19 +3,20 @@ $module = Yii::app()->getController()->module->id;
 $baseURL = Yii::app()->request->baseUrl;
 $document = $this->document;
 $text = $this->text;
-$link = "toto";
+$link = "";
+$imageSize = 30;
 if (isset($document) && isset($document['name']) && isset($document['folder'])) {
 	$name = strtolower($document['name']);
 	if(strrpos($name, ".pdf") != false)
 		$link = '<a href="'.$baseURL."/upload/".$module."/".$document['folder']."/".$document['name'].'" target="_blank">'.
-					'<i class="fa fa-file-pdf-o fa-3x icon-big"></i>'.$text.'</a>';	
+					'<i class="fa fa-file-pdf-o fa-2x"></i>'.$text.'</a>';	
 	else if( strrpos( $name, ".jpg" ) != false || strrpos($name, ".jpeg") != false || strrpos($name, ".gif")  != false || strrpos($name, ".png")  != false  )
 		$link = '<a href="'.$baseURL."/upload/".$module."/".$document['folder']."/".$document['name'].'" data-lightbox="docs">'.
-					'<img width="50" class="" src="'.Yii::app()->request->baseUrl."/upload/".$module."/".$document['folder']."/".$document['name'].'"/>'.
+					'<img class="" src="'.Yii::app()->request->baseUrl."/".$module."/document/resized/25x25/upload/".$module."/".$document['folder']."/".$document['name'].'"/>'.
 					$text.'</a>';	
 	else
 		$link = '<a href="'.$baseURL."/upload/".$module."/".$document['folder']."/".$document['name'].'" target="_blank">'.
-					'<i class="fa fa-file fa-3x icon-big"></i>'.$text.'</a>';
+					'<i class="fa fa-file fa-2x"></i>'.$text.'</a>';
 } else {
 	throw new CTKException("The document is not well formated");
 }
