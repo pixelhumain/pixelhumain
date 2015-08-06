78f9a9928129ed529f510714363f755cf62f3cd0
Sylvain Barbot <sylvain.barbot@gmail.com>
2015-08-06 15:16:46 +0400

    Fix the validation email + new parameter to force email sending

--- ph/protected/config/paramsconfig.php
+++ ph/protected/config/paramsconfig.php
@@ -3,11 +3,11 @@ $mailConfig = array(
 	'class' => 'ext.mail.YiiMail',
 	'transportType' => 'smtp',
     'transportOptions'=>array(
-	  'host'=>'smtp.gmail.com',
+	  'host'=>'',
       'username'=>'',
       'password'=>'',
       'port'=>'465',
-      'encryption'=>'ssl',
+      'encryption'=>'tls',
     ),
 	'viewPath' => 'application.views.emails',
 	'logging' => true,
@@ -30,20 +30,30 @@ $mailConfigTest = array(
 );
 
 $params = array(
-    // this is used in contact page
-    'adminEmail'=>'contact@pixelhumain.com',
+    //Beta Test ?
+    'betaTest' => true,
+
+    //By default controller
+    'defaultController' => 'communecter',
+
+    // Mail configuration
+    'adminEmail'=>'testmail.pixelhumain@gmail.com',
+    'forceMailSend' => true,
+
     //upload base directory
     'uploadUrl' => "upload/",
     'uploadDir' => dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR."..".DIRECTORY_SEPARATOR."upload".DIRECTORY_SEPARATOR,
     'uploadComDir' => dirname(__FILE__).DIRECTORY_SEPARATOR.'..'."\\..\\templates\\upload\\dir\\communecter\\collection\\person",
+    
+    //Captcha
     'captcha' => '6LdiygUTAAAAAEsbbK7LvMjJRt9PLP9lO-6QSM8K',
     'captcha-key' => '6LdiygUTAAAAAKZxZ0c9-G43Xqp9ZiedhWswto1s',
+    
+    //External API Key
     'facebook' => array('idAPP' => '974944225849158',
                         'secretAPP' => "3e485c0809b44e953963c778f5a3c2a0",
                         'required_scope' => 'public_profile, publish_actions, read_custom_friendlists, user_groups, user_likes, publish_pages'),
     'mandrill' => '4eD8BtmL5L_Z0E7Zz69Zlw',
-    'defaultController' => 'communecter',
-    'betaTest' => false
   );
 
 $modulesDir = '/../../../../modules/';
