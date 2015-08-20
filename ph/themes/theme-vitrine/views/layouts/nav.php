46ab79a2302cf5d6e700cc8eb3a7d2f93bf1027d
Robert Johnson <zeriam974@gmail.com>
2015-08-20 11:09:23 +0400

    #Vitrine Gestions des ancres

--- ph/themes/theme-vitrine/views/layouts/nav.php
+++ ph/themes/theme-vitrine/views/layouts/nav.php
@@ -8,14 +8,14 @@
 <nav id="navigation">
 	<div class="container">
 		<!-- LOGO GOES HERE -->
-		<a href="#" id="logo"><strong>PIXEL HUMAIN</strong></a>
+		<a data-href="#sectionsvg" id="logo"><strong>PIXEL HUMAIN</strong></a>
 		<!-- MENU -->
 		<nav>
 			<ul id="menu">
-				<li><a class="nav_label" href="#description">LE PROJET</a></li>
-				<li><a class="nav_label" id="label_team" href="#team">LE RESEAU</a></li>
-				<li><a href="#services" class="btn btn-success">JE PARTICIPE</a></li>
-				<li><a class="nav_label" id="label_cont"href="#contact">CONTACT</a></li>
+				<li><a class="nav_label" data-href="#description">LE PROJET</a></li>
+				<li><a class="nav_label" id="label_team" data-href="#team">LE RESEAU</a></li>
+				<li><a data-href="#services" class="btn btn-success">JE PARTICIPE</a></li>
+				<li><a class="nav_label" id="label_cont" data-href="#contact">CONTACT</a></li>
 					<!--<li><a href="javascript:;" class ="btn btnCom btn-success" id="btnCom" data-toggle="modal" data-target="#smallModal">COMMENTAIRE</a></li>-->
 					
 			</ul>
