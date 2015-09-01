d81e89c3a1240bfea2ddb1ba9b876ecf970a0b8f
Sylvain Barbot <sylvain.barbot@gmail.com>
2015-09-01 19:45:03 +0400

    add validation key on validation email

--- ph/protected/views/emails/invitation.php
+++ ph/protected/views/emails/invitation.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+  <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
 <html>
   <head>
     <title>PixelHumain</title>
@@ -10,13 +10,19 @@
         <td align="left" valign="top">
           <img src='<?php echo Yii::app()->getRequest()->getBaseUrl(true); ?>/images/logo/logo144.png' alt="PixelHumain" title="PixelHumain"/>
         </td>
+        <?php 
+          $validationKey =Person::getValidationKeyCheck($invitedUserId);
+          $url = Yii::app()->getRequest()->getBaseUrl(true)."/".$this->module->id."/person/activate/user/".$invitedUserId.'/validationKey/'.$validationKey;
+        ?>
         <td align="left">
            <h3>Bienvenue sur Communecter !! </h3>
-           Vous avez été invité au projet Pixel Humain par <?php echo $sponsorName?><br/>
-           Veuillez clicker sur le lien pour participer a cette initiative locale pour améliorer votre commune.<br/>
+           Vous avez été invité au projet Communecter par <?php echo $sponsorName?><br/>
+           Veuillez clicker sur ce lien pour participer a cette initiative locale pour améliorer votre commune.<br/>
            ou copier le directement dans votre navigateur.
            <br/>
-           <a href="<?php echo Yii::app()->getRequest()->getBaseUrl(true).'/'.$this->module->id?>/login?>">Confirmer L'invitation</a>
+           <a href="<?php echo $url?>">Confirmer L'invitation</a>
+           <br/>
+           <?php echo $url?>
         </td>
       </tr>
       </table>
