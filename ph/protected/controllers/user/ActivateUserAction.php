5aac296b8925ec2ea1ac797dba64e6a349ab17ab
Sylvain Barbot <sylvain.barbot@gmail.com>
2015-09-14 16:58:00 +0400

    add user validation on mail + redirect

--- ph/protected/controllers/user/ActivateUserAction.php
+++ ph/protected/controllers/user/ActivateUserAction.php
@@ -3,11 +3,19 @@ class ActivateUserAction extends CAction
 {
     public function run()
     {
+        $controller=$this->getController();
         $id = $_REQUEST["id"];
 		$user = Yii::app()->mongodb->citoyens->findAndModify( array("_id" => new MongoId( $id )), 
                                                               array('$set' => array("status"=>1) ) );
-        //$user = Yii::app()->mongodb->citoyens->findOne( array( "email" => $email ) );
-        Rest::json( $user );
-        Yii::app()->end();
+        
+        if (!$user) {
+        	//Unknown user
+        	$params=array("/azotlive?userValidated=0");
+        } else {
+        	//User validated
+        	$params=array("/azotlive?userValidated=1");
+        }
+        
+        $controller->redirect($params);
     }
 }
--- ph/protected/views/emails/validation.php
+++ ph/protected/views/emails/validation.php
@@ -12,14 +12,14 @@
     <table cellpadding="0" cellspacing="0" border="0" width="100%">
       <tr>
         <td align="left" valign="top">
-          <img src='<?php echo Yii::app()->getRequest()->getBaseUrl(true).$logo ?>' alt="<?php echo $title?>" title="<?php echo $title?>"/>
+          <img src='<?php echo $logo ?>' alt="<?php echo $title?>" title="<?php echo $title?>"/>
         </td>
         <td align="left">
            <h3>Bienvenue au <?php echo $title?> !! </h3>
            veuillez clicker sur le lien pour confirmer votre compte<br/>
            ou copier le directement dans votre navigateur 
            <br/>
-           <a href="<?php echo Yii::app()->getRequest()->getBaseUrl(true); ?>/citoyens/activate/user/<?php echo $user?>">Validation</a>
+           <a href="<?php echo Yii::app()->getRequest()->getBaseUrl(true)."/".$this->module->id; ?>/api/activateUser/id/<?php echo $user?>">Validation</a>
         </td>
       </tr>
       </table>
