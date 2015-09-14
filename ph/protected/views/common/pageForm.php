2e88c5070a008df5e0c29bd54e2e35268dfb7d41
Tibor Katelbach <oceatoon@gmail.com>
2015-09-14 23:16:12 +0400

    clean changes

--- ph/protected/views/common/pageForm.php
+++
@@ -1,5 +0,0 @@
-<!-- All details of a web page -->
-title : <input type="text" name='title' value="tintin"/><br/>
-link : <input type="text" name='link' value="linlin"/><br/>
-state : <input type="text" name='state' value="zinzin"/><br/>
-   
--- ph/protected/views/tools/tchat.php
+++
@@ -1,3 +0,0 @@
-<div  class="row">
-<iframe src="https://kiwiirc.com/client/irc.freenode.net/#pixelhumain" style="border:0; width:100%; height:450px;"></iframe>
-</div>
