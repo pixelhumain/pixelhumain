7bbee4090010b537f46b213edc259e92b588a71a
Sylvain Barbot <sylvain.barbot@gmail.com>
2015-09-23 08:40:54 +0400

    add new citybox theme

---
+++ ph/themes/citybox/views/layouts/mainSimple.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<!-- Template Name: Rapido - Responsive Admin Template build with Twitter Bootstrap 3.x Version: 1.0 Author: ClipTheme -->
+<!--[if IE 8]><html class="ie8 no-js" lang="en"><![endif]-->
+<!--[if IE 9]><html class="ie9 no-js" lang="en"><![endif]-->
+<!--[if !IE]><!-->
+<html lang="en" class="no-js">
+	<!--<![endif]-->
+	<!-- start: HEAD -->
+	<head>
+		<?php 
+		$layoutPath = 'webroot.themes.'.Yii::app()->theme->name.'.views.layouts.';
+		$this->renderPartial($layoutPath.'metas');?>
+		<!-- end: META -->
+		<!-- start: MAIN CSS -->
+		<?php 
+		$themeAssetsUrl = Yii::app()->theme->baseUrl. '/assets';
+
+		$cs = Yii::app()->getClientScript();
+		$cs->registerCssFile($themeAssetsUrl. '/plugins/bootstrap/css/bootstrap.min.css');
+		$cs->registerCssFile($themeAssetsUrl. '/plugins/ladda-bootstrap/dist/ladda.min.css');
+		$cs->registerCssFile($themeAssetsUrl. '/plugins/ladda-bootstrap/dist/ladda-themeless.min.css');
+		$cs->registerCssFile($themeAssetsUrl. '/plugins/font-awesome/css/font-awesome.min.css');
+		$cs->registerCssFile($themeAssetsUrl. '/plugins/font-awesome-custom/css/font-awesome.css');
+		$cs->registerScriptFile($themeAssetsUrl. '/plugins/velocity/jquery.velocity.min.js' , CClientScript::POS_END);
+		$cs->registerScriptFile($themeAssetsUrl. '/js/subview.js' , CClientScript::POS_END);
+		$cs->registerScriptFile($themeAssetsUrl. '/js/subview-examples.js' , CClientScript::POS_END);
+		$cs->registerCssFile($themeAssetsUrl. '/plugins/animate.css/animate.min.css');
+		$cs->registerCssFile($themeAssetsUrl. '/plugins/iCheck/skins/all.css');
+		$cs->registerCssFile($themeAssetsUrl. '/css/styles.css');
+		$cs->registerCssFile($themeAssetsUrl. '/css/styles-responsive.css');
+		$cs->registerCssFile($themeAssetsUrl. '/css/plugins.css');
+		$cs->registerCssFile($themeAssetsUrl. '/plugins/iCheck/skins/all.css');
+		$cs->registerCssFile($themeAssetsUrl. '/plugins/toastr/toastr.min.css');
+		$cs->registerCssFile($themeAssetsUrl.'/plugins/bootstrap-fileupload/bootstrap-fileupload.min.css');
+		$cs->registerScriptFile($themeAssetsUrl.'/plugins/bootstrap-fileupload/bootstrap-fileupload.min.js' , CClientScript::POS_END);
+		?>
+		<link rel='shortcut icon' type='image/x-icon' href="<?php echo (isset( $this->module->assetsUrl ) ) ? $this->module->assetsUrl : ""?>/images/favicon.ico" />
+		<!-- end: MAIN CSS -->
+		<!-- start: CSS REQUIRED FOR THIS PAGE ONLY -->
+		<!-- end: CSS REQUIRED FOR THIS PAGE ONLY -->
+		<script>
+		   var initT = new Object();
+		   var showDelaunay = true;
+		   var baseUrl = "<?php echo Yii::app()->getRequest()->getBaseUrl(true);?>";
+		   var themeUrl = "<?php echo Yii::app()->theme->baseUrl;?>";
+		   var moduleId = "<?php echo $this->module->id?>";
+		   var userId = "<?php echo Yii::app()->session['userId']?>";
+		   var debug = <?php echo (YII_DEBUG) ? "true" : "false" ?>;
+		   jQuery(document).ready(function() {
+				toastr.options = {
+				  "closeButton": false,
+				  "positionClass": "toast-bottom-right",
+				  "onclick": null,
+				  "showDuration": "1000",
+				  "hideDuration": "1000",
+				  "timeOut": "5000",
+				  "extendedTimeOut": "1000",
+				  "showEasing": "swing",
+				  "hideEasing": "linear",
+				  "showMethod": "fadeIn",
+				  "hideMethod": "fadeOut"
+				};
+			});
+		</script>
+		<style type="text/css">
+			.form-group label.error{ color:red; font-size:10px; }
+		</style>
+	</head>
+	<!-- end: HEAD -->
+	<!-- start: BODY -->
+	<body class="login bgcity">
+		
+		<?php 	$layoutPath = 'webroot.themes.'.Yii::app()->theme->name.'.views.layouts.';
+					$this->renderPartial($layoutPath.'mainMap');  ?>
+		
+		
+		<?php echo $content;  ?>
+		<!-- start: MAIN JAVASCRIPTS -->
+		<?php
+		echo "<!-- start: MAIN JAVASCRIPTS -->";
+		echo "<!--[if lt IE 9]>";
+		$cs->registerScriptFile(Yii::app()->theme->baseUrl. '/assets/plugins/respond.min.js' , CClientScript::POS_HEAD);
+		$cs->registerScriptFile(Yii::app()->theme->baseUrl. '/assets/plugins/excanvas.min.js' , CClientScript::POS_HEAD);
+		$cs->registerScriptFile(Yii::app()->theme->baseUrl. '/assets/plugins/jQuery/jquery-1.11.1.min.js' , CClientScript::POS_HEAD);
+		echo "<![endif]-->";
+		echo "<!--[if gte IE 9]><!-->";
+		$cs->registerScriptFile(Yii::app()->theme->baseUrl. '/assets/plugins/jQuery/jquery-2.1.1.min.js' , CClientScript::POS_HEAD);
+		echo "<!--<![endif]-->";
+		$cs->registerScriptFile(Yii::app()->theme->baseUrl. '/assets/plugins/jquery-ui/jquery-ui-1.10.2.custom.min.js' , CClientScript::POS_END);
+		$cs->registerScriptFile(Yii::app()->theme->baseUrl. '/assets/plugins/bootstrap/js/bootstrap.min.js' , CClientScript::POS_END);
+		$cs->registerScriptFile(Yii::app()->theme->baseUrl. '/assets/plugins/ladda-bootstrap/dist/spin.min.js' , CClientScript::POS_END);
+		$cs->registerScriptFile(Yii::app()->theme->baseUrl. '/assets/plugins/ladda-bootstrap/dist/ladda.min.js' , CClientScript::POS_END);
+		$cs->registerScriptFile(Yii::app()->theme->baseUrl. '/assets/plugins/iCheck/jquery.icheck.min.js' , CClientScript::POS_END);
+		$cs->registerScriptFile(Yii::app()->theme->baseUrl. '/assets/plugins/jquery.transit/jquery.transit.js' , CClientScript::POS_END);
+		$cs->registerScriptFile(Yii::app()->theme->baseUrl. '/assets/plugins/TouchSwipe/jquery.touchSwipe.min.js' , CClientScript::POS_END);
+		$cs->registerScriptFile(Yii::app()->theme->baseUrl. '/assets/plugins/bootbox/bootbox.min.js' , CClientScript::POS_END);
+		$cs->registerScriptFile(Yii::app()->theme->baseUrl. '/assets/plugins/jquery-mockjax/jquery.mockjax.js' , CClientScript::POS_END);
+		$cs->registerScriptFile(Yii::app()->theme->baseUrl. '/assets/plugins/blockUI/jquery.blockUI.js' , CClientScript::POS_END);
+		$cs->registerScriptFile(Yii::app()->theme->baseUrl. '/assets/plugins/toastr/toastr.js' , CClientScript::POS_END);
+
+		$cs->registerScriptFile(Yii::app()->theme->baseUrl. '/assets/js/jquery.dynForm.js' , CClientScript::POS_END);
+		$cs->registerScriptFile(Yii::app()->request->baseUrl.'/js/api.js' , CClientScript::POS_END);
+		$cs->registerScriptFile(Yii::app()->theme->baseUrl. '/assets/js/main.js' , CClientScript::POS_END);
+
+		$cs->registerCssFile(Yii::app()->theme->baseUrl. '/assets/plugins/select2/select2.css');
+		$cs->registerScriptFile(Yii::app()->theme->baseUrl. '/assets/plugins/select2/select2.min.js' , CClientScript::POS_END);
+
+		
+		//<!-- end: MAIN JAVASCRIPTS -->
+		//<!-- start: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
+		?>
+		<!-- end: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
+
+		
+	</body>
+	<!-- end: BODY -->
+</html>
