0652bd10b76607b4a0177da2cd33db99fda93bcd
Kgneo <tristan.goguet@gmail.com>
2015-09-28 15:49:28 +1100

    btn home + dropdown fix + pagination

--- ph/themes/ph-dori/views/layouts/mainMap.php
+++ ph/themes/ph-dori/views/layouts/mainMap.php
@@ -18,14 +18,14 @@
         "mapAttributions" => '<a href="http://www.opencyclemap.org">OpenCycleMap</a>',	 	//'Map tiles by <a href="http://stamen.com">Stamen Design</a>'
 
         /* MAP BUTTONS */
-        "mapBtnBgColor" => 'rgba(76, 114, 126, 0.65)', //'#E6D414',
+        "mapBtnBgColor" => '#4C727E', //'rgba(76, 114, 126, 0.65)', //'#E6D414',
         //"mapBtnColor" => 'rgba(76, 114, 126, 0.65)', //'#213042',
         //"mapBtnBgColor_hover" => 'rgba(76, 114, 126, 0.65)', //'#5896AB',
 
         /* USE */
         "titlePanel" 		 => '',
-        "usePanel" 			 => false,
-        "useFilterType" 	 => false,
+        "usePanel" 			 => true,
+        "useFilterType" 	 => true,
         "useRightList" 		 => true,
         "useZoomButton" 	 => true,
         "useHomeButton" 	 => true,
@@ -66,32 +66,44 @@
 		left:0px;
 	}
 	.<?php echo $moduleName; ?> .input-search-place{
-		right:unset;
+	/*	right:unset;
 		left:100px !important;
 		margin-right: 30px !important;
 		max-width: 254px !important;
-		z-index:100!important;
+		z-index:100!important;*/
 	}
 	.<?php echo $moduleName; ?> .input-search-place input.input-search-place-in-map{
 		background-color: rgba(111, 161, 177, 0.74) !important;
 	}
-	.<?php echo $moduleName; ?> .btn-group-map		{
-		left: unset;
+	.<?php echo $moduleName; ?> .btn-group-map{
 		top:50px !important;
-		right:30px;
+	}
+	.<?php echo $moduleName; ?> .btn-group-map.tools-btn{
+		left: unset; /*90px !important;*/
+		top:50px !important;
 		/*background-color: rgba(76, 114, 126, 0.65) !important;*/
 	}
-	.<?php echo $moduleName; ?> .item_panel_map			{
-	}
-	.<?php echo $moduleName; ?> .item_panel_map:hover	{
+	.<?php echo $moduleName; ?> .btn-group-map.tools-btn.input-search-place{
+		left: 90px !important;
+		top:50px !important;
+		/*background-color: rgba(76, 114, 126, 0.65) !important;*/
 	}
 
-	.<?php echo $moduleName; ?> #right_tool_map		{
-
+	.<?php echo $moduleName; ?> .panel_map, .panel_filter{
+		background:#4C727E;
 	}
-	.<?php echo $moduleName; ?> #liste_map_element	{}
+	.<?php echo $moduleName; ?> .item_panel_map{
+	}
+	.<?php echo $moduleName; ?> .item_panel_map:hover{
+	}
 
-	.<?php echo $moduleName; ?> #lbl-chk-scope		{}
+	.<?php echo $moduleName; ?> #right_tool_map{
+		top:50px;
+		z-index: 100;
+	}
+	.<?php echo $moduleName; ?> #liste_map_element{}
+
+	.<?php echo $moduleName; ?> #lbl-chk-scope{}
 
 	li.mix{
 		background:rgba(255, 255, 255, 0.3) !important;
@@ -147,10 +159,9 @@
 			marker.on('click', function(e) {
 					marker.openPopup();
 					//url = "showAjaxPanel( baseUrl+'/'+moduleId+'/".$type."/detail/id/".$id."', '".$type." : ".$name."','".$icon."' )";
-					alert(option.id);
 					if("undefined" != options.type) {
 						var type = options.type;
-						//showAjaxPanel( baseUrl+'/'+moduleId+'/' + type + '/detail/id/' + options.id, type + ' : ' + options.name, '' );
+						showAjaxPanel( baseUrl+'/'+moduleId+'/' + type + '/detail/id/' + options.id, type + ' : ' + options.name, '' );
 					}
 			});
 			return marker;
@@ -196,35 +207,41 @@
 				thisSig.showOneElementOnMap(data, thisMap);
 			}
 
+			
 			var points = L.geoJson(this.geoJsonCollection, {				//Pour les clusters seulement :
 					onEachFeature: function (feature, layer) {				//sur chaque marker
 						var id = feature.properties.id;
 						////console.dir(feature);
 						layer.setIcon(feature["properties"]["icon"]);	   	//affiche l'icon demandé
 						layer.on('click', function(e) {							
-							$("a[data-id='"+id+"']").click();
+							$(thisSig.cssModuleName + " .item_map_list_" +id).click();
 						});
 						
 						//au click sur un element de la liste de droite, on zoom pour déclusturiser, et on ouvre la bulle
 						$(thisSig.cssModuleName + " .item_map_list_" + feature.properties.id).click(function(){
-							thisMap.setView([	feature.geometry.coordinates[1],
+							thisSig.centerSimple([	feature.geometry.coordinates[1],
 										  		feature.geometry.coordinates[0]],
 										  		13, {"animate" : true });
 
-							var onclick = getActionsById(id); //$("a[data-id='"+id+"']").attr('onclick');
-							//console.log(onclick);
-							//showAjaxPanel( baseUrl+'/'+moduleId+'/organization/detail/id/54eed32ca1aa143e020041c4', 'organization : TEEO','fa-users' );
-							setTimeout(onclick, 1);
+							//var onclick = getActionsById(id); //$("a[data-id='"+id+"']").attr('onclick');
+							
+							var prop = feature.properties;
+							console.log("PROPRIETES : ");
+							console.dir(prop);
+
+							showMap(false);
+							showAjaxPanel( baseUrl+'/'+moduleId+'/'+prop.type+'/detail/id/'+prop.id, prop.type + ' : ' + prop.name, 'fa-'+prop.faIcon );
+							//setTimeout(onclick, 1);
 							$("#right_tool_map").hide('fast');
 
-							finalShowMarker();
+							//finalShowMarker();
 							thisSig.checkListElementMap(thisMap);
 						});
-						////console.warn("--------------- showMapElements click OK  ---------------------");
+						//console.warn("--------------- showMapElements click OK  ---------------------");
 
 					}
 				});
-				////console.warn("--------------- showMapElements  onEachFeature OK ---------------------");
+				//console.warn("--------------- showMapElements  onEachFeature OK ---------------------");
 
 				this.markersLayer.addLayer(points); 		// add it to the cluster group
 				thisMap.addLayer(this.markersLayer);		// add it to the map
@@ -256,36 +273,39 @@
 
 		Sig.showIcoLoading(false);
 
-		var mapHeight = $("#mapCanvasBg").height();
-		var rightPanelHeight = mapHeight - 190;
-		$("#right_tool_map").css({"top":"120px"});
-		$("#right_tool_map").css({"height":rightPanelHeight});
-		$("#liste_map_element").css({"height":rightPanelHeight-50});
-
 		$("#right_tool_map").hide('fast');
-		$(".sigModuleBg").css("display","none");
-		showMap();
+		
+
+		showMap(false);
 	});
 
 
 	function showMap(show){
-		show = $(".sigModuleBg").css("display") == "none";
+		console.log(show);
+		if(show === undefined) show = $("#ajaxSV").css("display") != "none";
+		console.log(show);
+		
 		if(show){
-			//$("#right_tool_map").addClass("hidden");
 			
-			$(".sigModuleBg").show( 1000 );
+			$(".sigModuleBg").show( 700 );
+			$(".btn-group-map").show( 700 );
+			$("#ajaxSV").hide( 700 );
+			$("#right_tool_map").show(700);
 			$(".box-ajax").css({backgroundColor:'rgba(255, 255, 255, 0.5)'});
 			$(".box-ajax .mix").css({backgroundColor:'rgba(255, 255, 255, 0.5)'});
 			$("a.text-white").css({color:'#58879B'});
-			$('#btn-close-panel').show("fast");
 			
 		}else{
-			$(".sigModuleBg").hide( 1000 );
+			
+			$(".btn-group-map").hide( 700 );
+			$("#ajaxSV").show( 700 );
+			$("#right_tool_map").hide(700);
+			$(".panel_map").hide(1);
 			$(".box-ajax").css({backgroundColor:'rgba(255, 255, 255, 1)'});
 			$(".box-ajax .mix").css({backgroundColor:'rgba(255, 255, 255, 1)'});
-			$("a.text-white").css({color:'#FFF'});
-			$('#btn-close-panel').hide("fast");
+			
 		}
 	}
+
 
 </script>
