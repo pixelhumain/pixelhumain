501275c0e835bcd2cd9bd5dda75352d838b0181f
Sylvain Barbot <sylvain.barbot@gmail.com>
2015-10-06 18:00:32 +0400

    add jobs i18n

--- ph/protected/messages/fr/jobs.php
+++ ph/protected/messages/fr/jobs.php
@@ -7,7 +7,7 @@
  */
 
 	return array(
-		"The job posting id is mandatory to retrieve the job posting !" =>  "L'id de l'offre d'emploi est obligatoire pour récupérer l'offre d'emploi!",
+		"The job posting id is mandatory to retrieve the job posting !" =>  "L'id de l'offre d'emploi est obligatoire pour récupérer l'offre d'emploi !",
 		"New Job Offer" => "Nouvel offre d'emploi",
 		"Fill the form"	=> "Remplissez le formulaire",
 		"Job Posting" => "Offres d'emploi",
@@ -19,6 +19,34 @@
 		"Error updating the Organization : address is not well formated !" => "Erreur en mettant à jour l'organisation : addresse incorrect !",
 		"Can not remove the job : you are not authorized to update that job offer !" => "Suppression de l'offre impossible : vous n'êtes pas autorisé à modifier l'offre d'emploi !",
 		"Your job offer has been deleted with success" =>"Votre Offre d'emploi a été supprimé avec succès.",
+
+		"Offer a Job" => "Proposition de formation / emploi",
+		"Job Title" => "Titre de la formation / emploi",
+		"Enter Job Title" => "Entrez un titre",
+		"Postal Code" => "Code postal",
+		"Enter Job Postal Code" => "Entrez le code postal",
+		"Start Date" => "Date de début",
+		"Enter Start Date" => "Entrez la date de début",
+		"Hiring organization" => "Association organisatrice",
+		"Enter Hiring organization" => "Entrez l'association",
+		"Responsibilities" => "Responsabilité",
+		"Enter Job Responsibilities" => "Entrez les responsabilités",
+		"Education Requirements" => "Connaissances requises",
+		"Enter Job Education Requirements" => "Entrez les connaissances requises",
+		"Job Location details" => "Détails du lieu",
+		"Enter Job Location" => "Entrez les détails du lieu",
+		"Work Hours" => "Horaires",
+		"Enter Work Hour" => "Entrez les horaires",
+		"Employment Type" => "Type de contrat",
+		"Enter Employment Type" => "Entrez le type de contrat",
+		"Base Salary" => "Salaire de base",
+		"Enter Base Salary" => "Entrez le salaire de base",
+		"Tags" => "Mots clés",
+		"Enter Tags" => "Entrez les mots clés",
+		"Description" => "Description",
+		"Enter Job Description" => "Entrez la description",
+		"Qualifications" => "Qualifications",
+		"Enter Job Qualifications" => "Entrez les qualifications",
 	);
 
 
