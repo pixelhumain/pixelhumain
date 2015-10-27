f762f92de381b6e2c48c3ada48203a754efb31cf
Childe <childeric_thoreau@hotmail.fr>
2015-10-27 18:11:25 +0400

    Ancienne version de payplug

--- ph/protected/extensions/paynplug/azotlive/cancel.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once("../lib/Payplug.php");
-$parametersFile = __DIR__ . "/params.json";
-$headers = getallheaders();
-$parameters = Parameters::loadFromFile($parametersFile);
-echo "in cancel";
-
