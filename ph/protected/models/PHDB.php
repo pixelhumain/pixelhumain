a89ee55a6db7c736f5b1a653b8334874c36526d0
Kgneo <tristan.goguet@gmail.com>
2015-11-06 17:20:03 +1100

    generic remove accent

--- ph/protected/models/PHDB.php
+++ ph/protected/models/PHDB.php
@@ -264,4 +264,17 @@ class PHDB
       }
       return $id;
     }
+
+    //supprime les accens d'une chaine de caractere
+    //utile pour les regex
+    public static  function wd_remove_accents($str, $charset='utf-8')
+    {
+        $str = htmlentities($str, ENT_NOQUOTES, $charset);
+        
+        $str = preg_replace('#&([A-za-z])(?:acute|cedil|caron|circ|grave|orn|ring|slash|th|tilde|uml);#', '\1', $str);
+        $str = preg_replace('#&([A-za-z]{2})(?:lig);#', '\1', $str); // pour les ligatures e.g. '&oelig;'
+        $str = preg_replace('#&[^;]+;#', '', $str); // supprime les autres caractères
+        
+        return $str;
+    }
 }
