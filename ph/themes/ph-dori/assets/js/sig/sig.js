86c7db54ddb70390f921dc335fe152f5a7a69047
Sylvain Barbot <sylvain.barbot@gmail.com>
2015-12-18 16:35:20 +0400

    use new tiles url for ssl

--- ph/themes/ph-dori/assets/js/sig/sig.js
+++ ph/themes/ph-dori/assets/js/sig/sig.js
@@ -43,7 +43,7 @@ var Sig = function() {
 		
 		
 		if(options.tileLayer == "toner")
-		L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
+		L.tileLayer('//stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png', {
 			attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
 			subdomains: 'abcd'
 		}).setOpacity(0.4).addTo(_MAP);
