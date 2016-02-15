861ef03a477dd7fee55e6be9bb1971111fd741b3
Sylvain Barbot <sylvain.barbot@gmail.com>
2016-02-15 18:49:28 +0400

    new email for contact form

---
+++ ph/protected/views/emails/newContactByEmail.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+<html>
+  <head>
+    <title>Nouveau Message depuis le site internet :</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  </head>
+  <body>
+    <table cellpadding="0" cellspacing="0" border="0" width="100%">
+      <tr>
+        <td align="left" valign="top">
+          <img src='<?php echo Yii::app()->getRequest()->getBaseUrl(true).$this->module->assetsUrl.$logo ?>' alt="Granddir" title="Granddir"/>
+        </td>
+        <td align="left">
+           <span>Nom : </span><?php echo $name?><br/>
+           <span>Email : </span><?php echo $email?><br/>
+           <span>Sujet : </span><?php echo $subject?><br/>
+           <span>Message : </span><?php echo $message?><br/>
+        </td>
+      </tr>
+    </table>
+  </body>
+</html>
