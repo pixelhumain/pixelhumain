36c24594376f611987a73ae2ded52b93c050cdf2
Tibor Katelbach <oceatoon@gmail.com>
2016-02-23 23:57:05 +0400

    added system ids for communecter and open atlas entities must be changed in prod, mail paths

--- ph/protected/config/paramsconfig.php
+++ ph/protected/config/paramsconfig.php
@@ -66,7 +66,9 @@ $params = array(
     //Functionnal params
     //Is the organisation bellow an othe organization can be managed by her top organization
     'isParentOrganizationAdmin' => true,
-    'adminUserId' => '520931e2f6b95c5cd3003d6c'
+    'adminUserId' => '520931e2f6b95c5cd3003d6c',
+    "openatlasId" => "54eed904a1aa1958b70041ef",
+    "communecterId" => "56c1a474f6ca47a8378b45ef"
   );
 
 $modulesDir = '/../../../../modules/';
