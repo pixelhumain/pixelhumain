b22041c0a456263ba780062285b6df136f7f237c
Clément Damiens <clement.damiens@gmail.com>
2016-06-07 15:21:45 +0200

    french trad

--- ph/protected/messages/fr/event.php
+++ ph/protected/messages/fr/event.php
@@ -2,19 +2,20 @@
 
 	return array(
 		"EVENT" => "Événement",
-		"Event's informations" => "Informations sur l'evenement",
-		"Event removed" => "Evenement bien supprimé",
+		"Event's informations" => "Informations sur l'événement",
+		"Event removed" => "Evénement bien supprimé",
 		"Invalid end-Date" => "Date de fin incorrect",
 		"Invalid date" => "Date incorrect",
 		"Event already exist" => "Cette Evenement existe déjà.",
-		"Event well updated" => "Votre evenement a ete modifié avec succes.",
+		"Event well updated" => "Votre événement a été modifié avec succès.",
 		"Create an Event" => "Créer un nouvel événement",
 		"Leave this Event" 		=> "Ne plus participer cet événement",
 		"Join this Event" 		=> "Participer à cet événement",
 		"Join" 					=> "Participer",
 		"Event Categories" => "Catégorie de l'événement",
 		"You organize an event ? Share it in the agenda !" => "Vous organisez un événement ? Partagez-le dans l'agenda !",
-		"Add an event"=> "Ajouter un événement"
+		"Add an event"=> "Ajouter un événement",
+		"Only people can attend to an event" => "Seul des citoyens peuvent participer à un évènement"
 	)
 
 ?>
