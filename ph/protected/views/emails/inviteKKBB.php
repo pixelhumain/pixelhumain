e64f78674dd62e7fe2e4b08028f19a107bc7e129
Sylvain Barbot <sylvain.barbot@gmail.com>
2016-06-08 18:28:34 +0400

    fix small bugs

--- ph/protected/views/emails/inviteKKBB.php
+++ ph/protected/views/emails/inviteKKBB.php
@@ -1460,7 +1460,7 @@
 							<center style="width: 100%;min-width: 532px;">
 								<a href="http://www.communecter.org" align="center" class="text-center" style="color: #e33551;font-family: Helvetica, Arial, sans-serif;font-weight: normal;padding: 0;margin: 0;text-align: left;line-height: 1.3;text-decoration: none;">
 								<!--http://localhost:8888/ph/images/logo-communecter.png-->
-								<img src="<?php echo Yii::app()->getRequest()->getBaseUrl(true).$logo ?>;" class="swu-logo" alt="Logo Image" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;width: 170px;max-width: 100%;clear: both;display: block;border: none;height: auto;padding: 15px 0px 0px 0px;">
+								<img src="<?php echo Yii::app()->getRequest()->getBaseUrl(true).$logo ?>" class="swu-logo" alt="Logo Image" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;width: 170px;max-width: 100%;clear: both;display: block;border: none;height: auto;padding: 15px 0px 0px 0px;">
 								</a>
 							</center>
                 		</th>
@@ -1474,7 +1474,7 @@
 				<table style="border-spacing: 0;border-collapse: collapse;padding: 0;vertical-align: top;text-align: left;width: 100%;">
 					<tr style="padding: 0;vertical-align: top;text-align: left;">
 						<th style="color: #3c5665;font-family: Helvetica, Arial, sans-serif;font-weight: normal;padding: 0;margin: 0;text-align: left;line-height: 19px;font-size: 15px;">
-							<h1 class="text-center" style="color: #e33551;font-family: Helvetica, Arial, sans-serif;font-weight: normal;padding: 35px 0px 15px 0px;margin: 0;text-align: center;line-height: 1.3;word-wrap: normal;margin-bottom: 10px;font-size: 34px;">Invitation sur communecter</h1>
+							<h1 class="text-center" style="color: #e33551;font-family: Helvetica, Arial, sans-serif;font-weight: normal;padding: 35px 0px 15px 0px;margin: 0;text-align: center;line-height: 1.3;word-wrap: normal;margin-bottom: 10px;font-size: 34px;">Derni&#232;res nouvelles sur communecter</h1>
 							<center style="width: 100%;min-width: 532px;">
 							<!--http://localhost:8888/ph/images/logoLTxt.jpg-->
 							<img src="<?php echo Yii::app()->getRequest()->getBaseUrl(true).$logo2 ?>" valign="bottom" alt="Logo Communecter" align="center" class="text-center" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;width: auto;max-width: 100%;clear: both;display: block;margin: 0 auto;float: none;text-align: center;">
@@ -1503,7 +1503,7 @@
 				    <tr style="padding: 0;vertical-align: top;text-align: left;">
 					    <th style="color: #3c5665;font-family: Helvetica, Arial, sans-serif;font-weight: normal;padding: 0;margin: 0;text-align: left;line-height: 19px;font-size: 15px;">
 					    	<!--http://localhost:8888/ph/images/betatest.png-->
-							<a href="#" style="color: #e33551;font-family: Helvetica, Arial, sans-serif;font-weight: normal;padding: 0;margin: 0;text-align: left;line-height: 1.3;text-decoration: none;"><img align="right" width="200" src="<?php echo Yii::app()>getRequest()->getBaseUrl(true)."images/bdb.png"?> style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;width: auto;max-width: 100%;clear: both;display: block;border: none;" alt="1+1=3"></a>
+							<a href="#" style="color: #e33551;font-family: Helvetica, Arial, sans-serif;font-weight: normal;padding: 0;margin: 0;text-align: left;line-height: 1.3;text-decoration: none;"><img align="right" width="200" src="<?php echo Yii::app()->getRequest()->getBaseUrl(true)."/images/bdb.png" ?>"" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;width: auto;max-width: 100%;clear: both;display: block;border: none;" alt="1+1=3"></a>
 							<p dir="ltr" style="margin: 0;margin-bottom: 10px;color: #777777 !important;font-family: Helvetica, Arial, sans-serif;font-weight: normal;padding: 0;text-align: left;line-height: 19px;font-size: 15px;">
 							    Cher Contributeur,
 							</p>
@@ -1513,18 +1513,18 @@
 							</p>
 							<br>
 							<p dir="ltr" style="margin: 0;margin-bottom: 10px;color: #777777 !important;font-family: Helvetica, Arial, sans-serif;font-weight: normal;padding: 0;text-align: left;line-height: 19px;font-size: 15px;">
-							    Comme vous avez pu le lire dans la derni&#232;re news, nous sommes sur le pont pour pr&#233;parer les contributions : t-shirts, cl&#233; USB, mugs, paniers
-							    gourmands&#8230; Bient&#244;t des instructions pour venir le chercher &#224; un de nos rendez-vous ou pour le recevoir dans votre boite aux lettres.
+							    Comme vous avez pu le lire dans la derni&#232;re news, nous sommes sur le pont pour pr&#233;parer les contributions : <strong>t-shirts, cl&#233; USB, mugs, paniers
+							    gourmands&#8230;</strong> Bient&#244;t des instructions pour venir le chercher &#224; un de nos rendez-vous ou pour le recevoir dans votre boite aux lettres.
 							</p>
 							<br>
 							<p dir="ltr" style="margin: 0;margin-bottom: 10px;color: #777777 !important;font-family: Helvetica, Arial, sans-serif;font-weight: normal;padding: 0;text-align: left;line-height: 19px;font-size: 15px;">
 							    Mais la contribution qui nous prend le plus de temps c&#8217;est la r&#233;alisation de notre site web de r&#233;seau social citoyen. Et l&#224; encore
-							    &#231;a avance &#224; grands pas : sa sortie est imminente.
+							    &#231;a avance &#224; grands pas : <strong>sa sortie est imminente</strong>.
 							</p>
 							<br>
 							<?php if ($isInvited) {?>
 							<p dir="ltr" style="margin: 0;margin-bottom: 10px;color: #777777 !important;font-family: Helvetica, Arial, sans-serif;font-weight: normal;padding: 0;text-align: left;line-height: 19px;font-size: 15px;">
-							    Nous sommes fiers de vous inviter &#224; rejoindre la plateforme en tant que beta testers. Vous serez les premiers &#224; venir profiter de votre outil
+							    <strong>Nous sommes fiers de vous inviter </strong>&#224; rejoindre la plateforme en tant que beta testers. Vous serez les premiers &#224; venir profiter de votre outil
 							    citoyen. Comment faire ?
 							</p>
 							<ol>
@@ -1560,12 +1560,12 @@
 							</ol>
 							<?php } else { ?>
 								<p dir="ltr" style="margin: 0;margin-bottom: 10px;color: #777777 !important;font-family: Helvetica, Arial, sans-serif;font-weight: normal;padding: 0;text-align: left;line-height: 19px;font-size: 15px;">
-						            Vous pouvez continuez à utiliser communecter et à alimenter votre réseau. Nous vous avons ajouté en tant que membre du groupe des 3C (Citoyens Contributeurs Commoners).
+						            Vous pouvez continuez à utiliser communecter et à alimenter votre réseau. Nous vous avons ajouté en tant que <strong>membre du groupe des 3C </strong>(Citoyens Contributeurs Commoners).
 							    </p>
 							<?php } ?>
 							<br>
 							<p dir="ltr" style="margin: 0;margin-bottom: 10px;color: #777777 !important;font-family: Helvetica, Arial, sans-serif;font-weight: normal;padding: 0;text-align: left;line-height: 19px;font-size: 15px;">
-							    Nous sommes en pr&#233;paration de vid&#233;os tutoriels pour vous aider &#224; vous familiariser avec l&#8217;outil. Mais nous l&#8217;avons voulu simple
+							    Nous sommes en pr&#233;paration de <strong>vid&#233;os tutoriels</strong> pour vous aider &#224; vous familiariser avec l&#8217;outil. Mais nous l&#8217;avons voulu simple
 							    d&#8217;utilisation et des bulles d&#8217;information apparaissent si vous laisser votre souris sur un bouton. De quoi vous aider d&#233;j&#224; &#224;
 							    vous rep&#233;rer.
 							</p>
@@ -1596,6 +1596,7 @@
     		</th>
         </tr></tbody></table>
 		<hr style="max-width: 580px;height: 0;border-right: 0;border-top: 0;border-bottom: 1px solid #cacaca;border-left: 0;margin: 20px auto;clear: both;">
+		<?php if ($isInvited) {?>
 		<table class="row" style="border-spacing: 0;border-collapse: collapse;padding: 0;vertical-align: top;text-align: left;width: 100%;position: relative;display: table;"><tbody><tr style="padding: 0;vertical-align: top;text-align: left;"> <!-- CTA -->
 			<th class="small-12 large-12 columns first last" style="color: #3c5665;font-family: Helvetica, Arial, sans-serif;font-weight: normal;padding: 0;margin: 0 auto;text-align: left;line-height: 19px;font-size: 15px;padding-left: 16px;padding-bottom: 16px;width: 564px;padding-right: 16px;">
       			<table style="border-spacing: 0;border-collapse: collapse;padding: 0;vertical-align: top;text-align: left;width: 100%;">
@@ -1613,8 +1614,9 @@
       			</table>
     		</th>
         </tr></tbody></table>
+    	<? }?>
     </td></tr></tbody></table> <!-- End main email content -->
-    
+   
 	<table class="container text-center" style="border-spacing: 0;border-collapse: collapse;padding: 0;vertical-align: top;text-align: center;background: #fefefe;width: 580px;margin: 0 auto;"><tbody><tr style="padding: 0;vertical-align: top;text-align: left;"><td style="word-wrap: break-word;-webkit-hyphens: auto;-moz-hyphens: auto;hyphens: auto;padding: 0;vertical-align: top;text-align: left;color: #3c5665;font-family: Helvetica, Arial, sans-serif;font-weight: normal;margin: 0;line-height: 19px;font-size: 15px;border-collapse: collapse !important;"> <!-- Footer -->
 		<table class="row grey" style="border-spacing: 0;border-collapse: collapse;padding: 0;vertical-align: top;text-align: left;background: #f0f0f0;width: 100%;position: relative;display: table;"><tbody><tr style="padding: 0;vertical-align: top;text-align: left;">
 			<th class="small-12 large-12 columns first last" style="color: #3c5665;font-family: Helvetica, Arial, sans-serif;font-weight: normal;padding: 0;margin: 0 auto;text-align: left;line-height: 19px;font-size: 15px;padding-left: 16px;padding-bottom: 16px;width: 564px;padding-right: 16px;">
