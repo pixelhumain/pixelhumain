b66a829f37f004499ec5ed0b07152cdafa379605
raphael <raphael_riviere@yahoo.fr>
2016-08-05 11:07:02 +0400

    mode test

--- ph/protected/config/main.php
+++ ph/protected/config/main.php
@@ -141,8 +141,8 @@ return array(
 			//'enableParamLogging' => YII_DEBUG,
 			'charset' => 'utf8'
 		),*/
-        'mongodb' => $dbconfig,
-		//'mongodb' => (($params['testActivated']==false)?$dbconfig:$dbconfigtest),
+        //'mongodb' => $dbconfig,
+		'mongodb' => $dbconfigtest,
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
