eed11490fe02ca2a9e4323e1695f088c41a209b0
tango <tristan.goguet@gmail.com>
2016-08-28 18:35:34 +1100

    up config

--- ph/protected/config/dbconfig.php
+++ ph/protected/config/dbconfig.php
@@ -2,7 +2,7 @@
 $dbconfig = array(
     'class' => 'mongoYii.EMongoClient',
     'server' => 'mongodb://127.0.0.1:27017/',
-    'db' => 'demo', 
+    'db' => 'pixelhumain', 
 );
 $dbconfigtest = array(
 	'class' => 'mongoYii.EMongoClient',
