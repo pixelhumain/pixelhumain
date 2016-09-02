ec3368c37fa631e2585d98f2d7888f749e7d498b
raphael <raphael_riviere@yahoo.fr>
2016-09-02 10:16:18 +0400

    AJout d'une traduction

--- ph/protected/messages/fr/common.php
+++ ph/protected/messages/fr/common.php
@@ -300,7 +300,6 @@ return array(
 	"Organizations" => "Organisations",
 	"Projects" => "Projets",
 	"Events" => "Événements",
-	"Cooperative space" => "Espace coopératif",
 	"of the event" => "de l'événement",
 	"of the organization" => "de l'organisation",
 	"of the project" => "du projet",
@@ -576,6 +575,9 @@ return array(
 	"go to documentation Index" => "Retour vers accueil documentation",
 	"Next chapter" => "Suivant",
 	"Previous chapter" => "Précédent",
+
+
+
 
 	"You are not connected to your city" => "Vous n'êtes pas communecté",
 	"To get quick access to information in your city, to filter and map view local content, please fill your postal code on your profile page." => "Pour obtenir un accès rapide à votre commune, et filtrer les résultats et la carte en fonction de celle-ci,
