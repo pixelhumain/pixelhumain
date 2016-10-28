9daf9911b3b93573ef6a6d20f853e59fe3191156
Sylvain Barbot <sylvain.barbot@gmail.com>
2016-10-28 15:17:20 +0400

    change default theme

--- ph/protected/config/main.php
+++ ph/protected/config/main.php
@@ -10,8 +10,8 @@ Yii::setPathOfAlias('Json',dirname(__FILE__) . '/../extensions/Json');
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Notragora',
-	'theme'=>'notragora',
+	'name'=>'Communecter',
+	'theme'=>'ph-dori',
 	'sourceLanguage'=>'00',
 	'language'=>'en',
 
