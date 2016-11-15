f9de482bce4ec4b9fb256ba85e53efe76cff867b
Clément Damiens <clement.damiens@gmail.com>
2016-11-15 12:50:12 +0100

    too many changes to describe (network first part)

---
+++ ph/themes/network/views/layouts/mainApi.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+<html lang="en" class="no-js">
+	<!--<![endif]-->
+	<!-- start: HEAD layout mainSearch.php -->
+	<head>
+		
+		<?php 
+		$themeAssetsUrl = Yii::app()->theme->baseUrl. '/assets';
+
+		$cs = Yii::app()->getClientScript();
+		?>
+	</head>
+	<!-- end: HEAD -->
+	<!-- start: BODY -->
+	<body class="swagger-section">
+
+	<?php 
+
+		echo $content;
+	
+		  ?>
+
+
+
+		<!-- start: MAIN JAVASCRIPTS -->
+		<?php
+			$cs->registerScriptFile(Yii::app()->theme->baseUrl. '/plugins/jQuery/jquery-2.1.1.min.js' , CClientScript::POS_HEAD);
+		?>
+		<!-- end: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
+	</body>
+	<!-- end: BODY -->
+</html>
