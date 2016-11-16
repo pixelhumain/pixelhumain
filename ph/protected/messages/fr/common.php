caa37c84104346d03455dfee7e02fdf2cdcb1f79
Raphael <raphael_riviere@yahoo.fr>
2016-11-16 15:11:33 +0400

    Ajout de traduction

--- ph/protected/messages/fr/common.php
+++ ph/protected/messages/fr/common.php
@@ -672,6 +672,12 @@ merci de renseigner votre code postal dans l'addresse de votre profil.",
 	"Add a primary address" => "Ajouter une adresse principal",
 	"Add a secondary address" => "Ajouter une adresse secondaire",
 	"Can not update the element : you are not authorized to update that element !" => "Impossible de mettre à jour cet élément : vous n'êtes pas autorisé !",
+
+	"I am in favor of this proposal" => "Je suis favorable à cette proposition",
+	"I think that this proposal is not complete" => "Je pense que cette proposition n'est pas complète",
+	"I have not reviews" => "Je n'ai pas d'avis",
+	"I don't understand, it miss informations" => "Je n'ai pas compris, il manque des informations",
+	"I am not in favor of this proposal" => "Je suis contre cette proposition"
 );
 
 ?>
