467784b20d93072906ebed15e74d26451a7acaa9
Tibor Katelbach <oceatoon@gmail.com>
2016-11-24 11:23:14 +0400

    cleanout chat

--- ph/themes/network/views/layouts/mainSearch.php
+++ ph/themes/network/views/layouts/mainSearch.php
@@ -127,13 +127,6 @@
 				<?php } ?>
 
 				
-				(function(w, d, s, u) {
-				    w.RocketChat = function(c) { w.RocketChat._.push(c) }; w.RocketChat._ = []; w.RocketChat.url = u;
-				    var h = d.getElementsByTagName(s)[0], j = d.createElement(s);
-				    j.async = true; j.src = 'https://chat.initiative.place/packages/rocketchat_livechat/assets/rocket-livechat.js';
-				    h.parentNode.insertBefore(j, h);
-				})(window, document, 'script', 'https://chat.initiative.place/livechat');
-				
 			});
 		</script>
 		<style type="text/css">
