e234a98c11a9cbb9af7a9a95997684fd7de20d55
Kgneo <tristan.goguet@gmail.com>
2016-12-05 16:30:47 +1100

    plug notif

--- ph/themes/kgougle/views/layouts/scopes/multi_tag_scope.php
+++ ph/themes/kgougle/views/layouts/scopes/multi_tag_scope.php
@@ -3,22 +3,11 @@
 
 <?php  HtmlHelper::registerCssAndScriptsFiles(array('/js/menus/multi_tags_scopes.js'), $this->module->assetsUrl); ?>
 
-<span data-tpl="default.scopes.multi_tag_scope">
+<span class="hidden" data-tpl="default.scopes.multi_tag_scope">
 <?php 
-$this->renderPartial($layoutPath.'scopes/multi_tag', array("me"=>$me)); 
-$this->renderPartial($layoutPath.'scopes/multi_scope', array("me"=>$me));
+	$this->renderPartial($layoutPath.'scopes/multi_tag', array("me"=>$me)); 
+	$this->renderPartial($layoutPath.'scopes/multi_scope', array("me"=>$me));
 ?>
-
-<?php  if( isset( Yii::app()->session['userId']) ){ ?>
-<button class="menu-button btn-menu btn-menu-notif tooltips text-dark" 
-      data-toggle="tooltip" data-placement="left" title="Notifications" alt="Notifications">
-  <i class="fa fa-bell"></i>
-  <span class="notifications-count topbar-badge badge badge-success animated bounceIn">
-  	<?php count($this->notifications); ?>
-  </span>
-</button>
-<?php } ?>
-
 </span>
 
 <script>
