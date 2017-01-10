068d0ca19a5f825f10ff066fceb9ea1387e8e71d
Clément Damiens <clement.damiens@gmail.com>
2017-01-10 13:16:32 +0100

    add jsonHelpers on js && add chart translation

---
+++ ph/protected/messages/fr/chart.php
@@ -0,0 +1,13 @@
+<?php
+
+	return array(
+		"Commons" => "Bien-commun",
+		"Open" => "Libre",
+		"Choose which kind of form to complete" => "Choisir le type de formulaire que vous souhaitez remplir",
+		"Add properties which defined your organization" => "Ajouter des propriétés qui définissent votre organisation",
+		"Add properties which defined your project" => "Ajouter des propriétés qui définissent votre projet",
+		"Evaluate your organization as commons" => "Evaluer votre organisation comme un bien commun",
+		"Evaluate your project as commons" => "Evaluer votre projet comme un bien commun",
+	)
+
+?>
