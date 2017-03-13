a1c2e65f9795d8294da27d46540fe514b333c521
Bouboule <clement.damiens@gmail.com>
2017-03-14 00:06:52 +0100

    fr trad

--- ph/protected/messages/fr/chart.php
+++ ph/protected/messages/fr/chart.php
@@ -8,6 +8,12 @@
 		"Add properties which defined your project" => "Ajouter des propriétés qui définissent votre projet",
 		"Evaluate your organization as commons" => "Evaluer votre organisation comme un bien commun",
 		"Evaluate your project as commons" => "Evaluer votre projet comme un bien commun",
+		"Edit properties" => "Editer les propriétés",
+		"Create Chart<br/>Opening<br/>Values<br/>Governance<br/>To explain the aim and draw project conduct" => "Créer un graphique montrant<br/>l'Ouverture<br/>les Valeurs<br/>la Gouvernance, etc<br/>Pour montrer le but et les valeurs du projets",
+		"Add project's properties" => "Ajouter des propriétés au projet",
+		"Degree of project's openness (0% = very closed, 100% = very opened)" => "Degré d'ouverture du projet (0% = très fermé, 100% = très ouvert)",
+		"Add<br/>A new<br/>Property" => "Ajouter<br/>Une nouvelle<br/>Propriété",
+		"Values and Cultures" => "Valeurs et Cultures", 
 	)
 
 ?>
