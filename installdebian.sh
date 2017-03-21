a1816f3aa99ea8e1b580dd1dd24f1bcbf3b703fe
tech62 <adrien.degardin@loos-en-gohelle.fr>
2017-03-21 20:21:57 +0100

    Update installdebian.sh

--- installdebian.sh
+++ installdebian.sh
@@ -47,139 +47,34 @@ mv pixelhumain/ph/protected/config/dbconfig.example.php pixelhumain/ph/protected
 
 echo "<VirtualHost 127.0.0.1:80>
 
-	# The ServerName directive sets the request scheme, hostname and port that
-
-	# the server uses to identify itself. This is used when creating
-
-	# redirection URLs. In the context of virtual hosts, the ServerName
-
-	# specifies what hostname must appear in the request's Host: header to
-
-	# match this virtual host. For the default virtual host (this file) this
-
-	# value is not decisive as it is used as a last resort host regardless.
-
-	# However, you must set it for any further virtual host explicitly.
-
-	#ServerName www.example.com
-
 	ServerName communecter.local
-
 	ServerAdmin webmaster@localhost
-
 	DocumentRoot /var/www/web/pixelhumain/ph
 
-
-
-	# Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
-
-	# error, crit, alert, emerg.
-
-	# It is also possible to configure the loglevel for particular
-
-	# modules, e.g.
-
-	#LogLevel info ssl:warn
-
-
-
 	ErrorLog ${APACHE_LOG_DIR}/error.log
-
 	CustomLog ${APACHE_LOG_DIR}/access.log combined
-
-
-
-	# For most configuration files from conf-available/, which are
-
-	# enabled or disabled at a global level, it is possible to
-
-	# include a line for only one particular virtual host. For example the
-
-	# following line enables the CGI configuration for this host only
-
-	# after it has been globally disabled with "a2disconf".
-
-	#Include conf-available/serve-cgi-bin.conf
-
-
-
-
 
 Alias "/ph" "/var/www/web/pixelhumain/ph"
 
 <Directory "/var/www/web/pixelhumain/ph">
 
 Options FollowSymLinks Indexes
-
 AllowOverride none
-
 Order deny,allow
-
 Allow from 127.0.0.1
-
 deny from all
 
 <IfModule mod_rewrite.c>
-
 Options +FollowSymLinks
-
 IndexIgnore */*
-
 RewriteEngine on
-
 RewriteCond %{REQUEST_FILENAME} !-f
-
 RewriteCond %{REQUEST_FILENAME} !-d
-
 RewriteCond $1 !^(index\.php|assets|robots\.txt)
-
 RewriteRule ^(.*)$ /ph/index.php/$1 [L]
-
 </IfModule>
 
 </Directory>
-
-
-
-Alias "/src" "/usr/share/squirrelmail"
-
-<Directory "/usr/share/squirrelmail">
-
-Options FollowSymLinks Indexes
-
-AllowOverride none
-
-Order deny,allow
-
-Allow from 127.0.0.1
-
-deny from all
-
-<IfModule mod_rewrite.c>
-
-Options +FollowSymLinks
-
-IndexIgnore */*
-
-RewriteEngine on
-
-RewriteCond %{REQUEST_FILENAME} !-f
-
-RewriteCond %{REQUEST_FILENAME} !-d
-
-RewriteCond $1 !^(index\.php|assets|robots\.txt)
-
-RewriteRule ^(.*)$ /ph/index.php/$1 [L]
-
-</IfModule>
-
-</Directory>
-
-
-
-
-
-
 
 </VirtualHost>" > /etc/apache2/sites-available/communecter.conf
 
