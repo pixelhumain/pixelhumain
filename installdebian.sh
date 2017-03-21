cdf7f05dc1042e1fb13d9322b712e2c0f78d0f75
tech62 <adrien.degardin@loos-en-gohelle.fr>
2017-03-21 20:05:41 +0100

    Update installdebian.sh

--- installdebian.sh
+++ installdebian.sh
@@ -17,7 +17,7 @@ mkdir /var/www/web && mkdir /var/www/web/modules && cd /var/www/web
 #Clonage des dépôts
 #Cloning git repo
 git clone https://github.com/pixelhumain/pixelhumain
-cd pixelhumain
+cd pixelhumain/ph
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
@@ -26,7 +26,7 @@ php -r "unlink('composer-setup.php');"
 
 php composer.phar update
 php composer.phar install
-cd ../
+cd ../../
 
 cd modules
 git clone https://github.com/pixelhumain/citizenToolKit
