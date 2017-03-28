ea7bd0143ddf05ff3e77cfe5a5ef47f84d334103
Sylvain Barbot <sylvain.barbot@gmail.com>
2017-03-28 15:04:58 +0400

    add test and composer

--- ph/tests/api/CreateNewOrgaCept.php
+++ ph/tests/api/CreateNewOrgaCept.php
@@ -1,9 +1,25 @@
 <?php 
 $I = new ApiTester($scenario);
-$I->wantTo('Create an organization');
+$I->wantTo('Create, get and delete an organization');
 $I->login("sylvain.barbot@gmail.com", "cobolisbad");
-$I->sendPOST('/element/save', ['name' => 'Une organization en test5', 'type' => 'LocalBusiness', 'role' => 'member', "collection" => "organizations", "key" => "organization" ]);
+$name = 'Une organization en test final';
+//Save new organization
+$I->sendPOST('/element/save', ['name' => $name, 'type' => 'LocalBusiness', 'role' => 'member', "collection" => "organizations", "key" => "organization" ]);
 //preferences[isOpenData]:true
 //preferences[isOpenEdition]:true
 $I->seeResponseCodeIs("200");
+$I->seeResponseContains('"result":true');
+$id = $I->grabDataFromResponseByJsonPath("$.id");
+
+//Get the element
+$I->sendGET('/element/get', ['type' => 'organizations', 'id' => $id[0]]);
+$I->seeResponseContains('"result":true');
+$I->seeResponseContains('"name":'.$name);
+
+//Delete the organization
+$I->sendGET('/element/delete', ['type' => 'organizations', 'id' => $id[0]]);
+$I->seeResponseContains('"result":true');
+
+//Get should return false
+$I->sendGET('/element/get', ['type' => 'organizations', 'id' => $id[0]]);
 $I->seeResponseContains('"result":false');
