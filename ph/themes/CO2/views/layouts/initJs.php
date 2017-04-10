3bc7ac2c8a93f0fcd7621f997e229cfadceb2f89
Oceatoon <oceatoon@gmail.com>
2017-04-10 10:42:44 +0400

    added config for POI , to use taglist in dynform

--- ph/themes/CO2/views/layouts/initJs.php
+++ ph/themes/CO2/views/layouts/initJs.php
@@ -33,6 +33,7 @@
     var classified = <?php echo json_encode( CO2::getContextList("classified") ) ?>;
     var place = <?php echo json_encode( CO2::getContextList("place") ) ?>;
     var ressource = <?php echo json_encode( CO2::getContextList("ressource") ) ?>;
+    var poi = <?php echo json_encode( CO2::getContextList("poi") ) ?>;
 
     //var classifiedSubTypes = <?php //echo json_encode( Classified::$classifiedSubTypes ) ?>;
     var urlTypes = <?php asort(Element::$urlTypes); echo json_encode(Element::$urlTypes) ?>;
