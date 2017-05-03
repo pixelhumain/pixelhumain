12bd4b23bd9b0f928932f03f00987095a7dca0cd
Bouboule <clement.damiens@gmail.com>
2017-05-03 20:01:40 +0200

    fix #11 and change banniere => banner

--- ph/protected/config/paramsconfig.php
+++ ph/protected/config/paramsconfig.php
@@ -87,6 +87,7 @@ $params = array(
     'mapboxToken' => 'pk.eyJ1IjoiY29tbXVuZWN0ZXIiLCJhIjoiY2lreWRkNzNrMDA0dXc3bTA1MHkwbXdscCJ9.NbvsJ14y2bMWWdGqucR_EQ',
 
     'CO2DomainName' => 'CO2', //used by CO2 theme, param header / css / js specific for each website using CO2 theme
+    "theme"=> "CO2",
 
     //rocketchat params
     'rocketchatEnabled' => false,
