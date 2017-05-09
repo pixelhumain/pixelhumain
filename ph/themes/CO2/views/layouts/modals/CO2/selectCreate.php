9064bffed538e8d5eba6c733fe950a7054891a81
Raphael <raphael_riviere@yahoo.fr>
2017-05-09 11:51:09 +0400

    hotfix dynForm type orga

--- ph/themes/CO2/views/layouts/modals/CO2/selectCreate.php
+++ ph/themes/CO2/views/layouts/modals/CO2/selectCreate.php
@@ -103,25 +103,25 @@
                         <small>Faire une proposition citoyenne<br>Participer à la démocratie locale<br>Être un citoyen actif</small>
                     </button>
                     -->
-                    <button data-form-type="organization" data-form-subtype="association"  data-dismiss="modal"
+                    <button data-form-type="organization" data-form-subtype="<?php echo Organization::TYPE_NGO; ?>"  data-dismiss="modal"
                             class="btn btn-link btn-open-form col-xs-6 col-sm-6 col-md-3 col-lg-3 text-green">
                         <h6><i class="fa fa-group fa-2x bg-green"></i><br> Association</h6>
                         <small>Faire connaitre votre association<br>Gérer les adhérents<br>Partager votre actualité</small>
                     </button>
                     
-                    <button data-form-type="organization" data-form-subtype="business"  data-dismiss="modal"
+                    <button data-form-type="organization" data-form-subtype="<?php echo Organization::TYPE_BUSINESS; ?>"  data-dismiss="modal"
                             class="btn btn-link btn-open-form col-xs-6 col-sm-6 col-md-3 col-lg-3 text-azure">
                         <h6><i class="fa fa-industry fa-2x bg-azure"></i><br> Entreprise</h6>
                         <small>Faire connaitre votre entreprise<br>Trouver de nouveaux clients<br>Gérer vos contacts</small>
                     </button>
                     
-                    <button data-form-type="organization" data-form-subtype="group"  data-dismiss="modal"
+                    <button data-form-type="organization" data-form-subtype="<?php echo Organization::TYPE_GROUP; ?>"  data-dismiss="modal"
                             class="btn btn-link btn-open-form col-xs-6 col-sm-6 col-md-3 col-lg-3 letter-turq">
                         <h6><i class="fa fa-group fa-2x bg-turq"></i><br> Groupe</h6>
                         <small>Créer un groupe<br>Partager vos centres d'intêrets<br>Discuter Communiquer S'amuser</small>
                     </button>
 
-                    <button data-form-type="organization" data-form-subtype="GovernmentOrganization"  data-dismiss="modal"
+                    <button data-form-type="organization" data-form-subtype="<?php echo Organization::TYPE_GOV; ?>"  data-dismiss="modal"
                             class="btn btn-link btn-open-form col-xs-6 col-sm-6 col-md-3 col-lg-3 text-green">
                         <h6><i class="fa fa-university fa-2x bg-green"></i><br> Service public</h6>
                         <small>Faire connaitre votre association<br>Gérer les adhérents<br>Partager votre actualité</small>
@@ -137,6 +137,7 @@
 jQuery(document).ready(function() { 
     $(".btn-open-form").click(function(){
         var typeForm = $(this).data("form-type");
+        mylog.log("test", $(this).data("form-subtype")),
         currentKFormType = ($(this).data("form-subtype")) ? $(this).data("form-subtype") : null;
         //alert(contextData.type+" && "+contextData.id+" : "+typeForm);
         if(contextData && contextData.type && contextData.id )
