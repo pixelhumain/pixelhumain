febe3c60efa499ca703958cec8109663cf1c2560
Raphael <raphael_riviere@yahoo.fr>
2017-05-10 15:19:36 +0400

    Modif cookie

--- ph/protected/extensions/helpers/CookieHelper.php
+++ ph/protected/extensions/helpers/CookieHelper.php
@@ -15,7 +15,7 @@ class CookieHelper  {
       {
         $cookie = new CHttpCookie($name,$value);
         $cookie->expire = time()+60*60*24*180;
-        $cookie->path = '/ph/co2';
+        $cookie->path = Yii::app()->baseUrl;
         Yii::app()->request->cookies[$name] = $cookie;
 
       }
