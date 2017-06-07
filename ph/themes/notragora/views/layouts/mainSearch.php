dee304782d759f39d0a7101bce90e1384a5a9d17
LassalleRemi <remil.lassalle@gmail.com>
2017-06-07 13:26:45 +0200

    Sly on menuTop

--- ph/themes/notragora/views/layouts/mainSearch.php
+++ ph/themes/notragora/views/layouts/mainSearch.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
-<?php 
-	
+<?php
+
 	/* COOKIE GEO POSITION */
 
  	/*	LISTE DES COOKIES
@@ -13,11 +13,11 @@
 	$user = "NOT_CONNECTED";
  	/*if(isset(Yii::app()->session['userId'])){
  		$user = Person::getById(Yii::app()->session['userId']);
-		
+
 		$user_geo_latitude = ""; $user_geo_longitude = "";
 		$insee = ""; $cityName = "";
 
-		if(isset($user["geo"]) && 
+		if(isset($user["geo"]) &&
  		   isset($user["geo"]["latitude"]) && isset($user["geo"]["longitude"]))
 		{
 			$user_geo_latitude = $user["geo"]["latitude"];
@@ -26,53 +26,54 @@
 
 		if(isset($user["address"]) && isset($user["address"]["codeInsee"]))
 			$insee = $user["address"]["codeInsee"];
-			
+
 		if(isset($user["address"]) && isset($user["address"]["addressLocality"]))
 			$cityName = $user["address"]["addressLocality"];
-			
+
 	}else{ //user not connected
 		if(isset($cookies['user_geo_longitude'])){
-				$sigParams["firstView"] = array(  "coordinates" => array( $cookies['user_geo_latitude']->value, 
+				$sigParams["firstView"] = array(  "coordinates" => array( $cookies['user_geo_latitude']->value,
 																		  $cookies['user_geo_longitude']->value),
-											 	  "zoom" => 13);		
+											 	  "zoom" => 13);
 		}else{
 			//error_log("aucun cookie geopos trouvé");
 		}
 	}*/
 
-?>	
+?>
 <html lang="en" class="no-js">
 	<!--<![endif]-->
 	<!-- start: HEAD layout mainSearch.php -->
 	<head>
-		<?php 
+		<?php
 
-		
+
 		$layoutPath = 'webroot.themes.'.Yii::app()->theme->name.'.views.layouts.';
 		$this->renderPartial($layoutPath.'metas');?>
 		<!-- end: META -->
 		<!-- start: MAIN CSS -->
-		<?php 
-		
+		<?php
+
 		$cs = Yii::app()->getClientScript();
 
 		$cs->registerScriptFile(Yii::app()->request->baseUrl.'/plugins/bootstrap-modal/js/bootstrap-modal.js' , CClientScript::POS_END);
 		$cs->registerScriptFile(Yii::app()->request->baseUrl.'/plugins/bootstrap-modal/js/bootstrap-modalmanager.js' , CClientScript::POS_END);
 		$cs->registerCssFile(Yii::app()->request->baseUrl.'/plugins/bootstrap-modal/css/bootstrap-modal.css' , CClientScript::POS_END);
-		
+
 		$cs->registerScriptFile(Yii::app() -> createUrl($this->module->id."/default/view/page/trad/dir/..|translation/layout/empty"));
 		?>
 		<link rel='shortcut icon' type='image/x-icon' href="<?php echo (isset( $this->module->assetsUrl ) ) ? $this->module->assetsUrl : ""?>/images/LogoNotragoraMiniature.ico" />
 		<link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl;?>/assets/css/themes/theme-simple.css" type="text/css" id="skin_color">
 		<link rel="stylesheet" href="<?php echo Yii::app()->theme->baseUrl;?>/assets/css/themes/theme-simple-login.css" type="text/css" id="skin_color">
 
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 		<script src='https://api.mapbox.com/mapbox.js/v2.4.0/mapbox.js'></script>
 		<link href='https://api.mapbox.com/mapbox.js/v2.4.0/mapbox.css' rel='stylesheet' />
-		
-		
+
+
 		<script src='//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js'></script>
         <link href='//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css' rel='stylesheet' />
-        
+
 		<!-- end: MAIN CSS -->
 		<!-- start: CSS REQUIRED FOR THIS PAGE ONLY -->
 		<!-- end: CSS REQUIRED FOR THIS PAGE ONLY -->
@@ -124,58 +125,58 @@
 	<!-- start: BODY -->
 	<body class="">
 		<!-- **************************************
-		INDEX structure 
+		INDEX structure
 		******************************************* -->
 
 	<?php
-	
+
 		$this->renderPartial( $layoutPath.'modals' );
-	
- 
+
+
 	//si l'utilisateur n'est pas connecté
  	if(!isset(Yii::app()->session['userId'])){
-		$inseeCommunexion 	 = isset( Yii::app()->request->cookies['inseeCommunexion'] ) ? 
+		$inseeCommunexion 	 = isset( Yii::app()->request->cookies['inseeCommunexion'] ) ?
 		   			    			  Yii::app()->request->cookies['inseeCommunexion']->value : "";
-		
-		$cpCommunexion 		 = isset( Yii::app()->request->cookies['cpCommunexion'] ) ? 
+
+		$cpCommunexion 		 = isset( Yii::app()->request->cookies['cpCommunexion'] ) ?
 		   			    			  Yii::app()->request->cookies['cpCommunexion']->value : "";
-		
-		$cityNameCommunexion = isset( Yii::app()->request->cookies['cityNameCommunexion'] ) ? 
+
+		$cityNameCommunexion = isset( Yii::app()->request->cookies['cityNameCommunexion'] ) ?
 		   			    			  Yii::app()->request->cookies['cityNameCommunexion']->value : "";
 
-		$regionNameCommunexion = isset( Yii::app()->request->cookies['regionNameCommunexion'] ) ? 
+		$regionNameCommunexion = isset( Yii::app()->request->cookies['regionNameCommunexion'] ) ?
 		   			    			  Yii::app()->request->cookies['regionNameCommunexion']->value : "";
 
-		$countryCommunexion = isset( Yii::app()->request->cookies['countryCommunexion'] ) ? 
+		$countryCommunexion = isset( Yii::app()->request->cookies['countryCommunexion'] ) ?
 		   			    			  Yii::app()->request->cookies['countryCommunexion']->value : "";
 	}
 	//si l'utilisateur est connecté
 	else{
 		$me = Person::getById(Yii::app()->session['userId']);
-		$inseeCommunexion 	 = isset( $me['address']['codeInsee'] ) ? 
+		$inseeCommunexion 	 = isset( $me['address']['codeInsee'] ) ?
 		   			    			  $me['address']['codeInsee'] : "";
-		
-		$cpCommunexion 		 = isset( $me['address']['postalCode'] ) ? 
+
+		$cpCommunexion 		 = isset( $me['address']['postalCode'] ) ?
 		   			    			  $me['address']['postalCode'] : "";
-		
-		$cityNameCommunexion = isset( $me['address']['addressLocality'] ) ? 
+
+		$cityNameCommunexion = isset( $me['address']['addressLocality'] ) ?
 		   			    			  $me['address']['addressLocality'] : "";
-		
+
 		$regionNameCommunexion = ""; /*not important now => multilevel is dead*/
 
-		$countryCommunexion = isset( $me['address']['addressCountry'] ) ? 
-		   			    			 $me['address']['addressCountry'] : "";	
+		$countryCommunexion = isset( $me['address']['addressCountry'] ) ?
+		   			    			 $me['address']['addressCountry'] : "";
 	}
 
 	if (@$inseeCommunexion){
 		if(@$cpCommunexion){
-			$city=City::getCityByInseeCp($inseeCommunexion, $cpCommunexion);	
+			$city=City::getCityByInseeCp($inseeCommunexion, $cpCommunexion);
 		}else{
 			$city=SIG::getCityByCodeInsee($inseeCommunexion);
 		}
 
 		if(@$me)
-		$regionNameCommunexion = @$city['regionName'] ? 
+		$regionNameCommunexion = @$city['regionName'] ?
 			   			    	 $city['regionName'] : "";
 
 		$nbCpByInsee=count(@$city["postalCodes"]);
@@ -187,7 +188,7 @@
 	}
 
 	?>
-	
+
 	<!-- **************************************
 	MAP CONTAINER
 	******************************************* -->
@@ -198,7 +199,7 @@
 	<?php //get all my link to put in floopDrawer
 		if(isset(Yii::app()->session['userId'])){
 	      $myContacts = Person::getPersonLinksByPersonId(Yii::app()->session['userId']);
-	      $myFormContact = $myContacts; 
+	      $myFormContact = $myContacts;
 	      $getType = (isset($_GET["type"]) && $_GET["type"] != "citoyens") ? $_GET["type"] : "citoyens";
 	    } else
 	      $myFormContact = null;
@@ -211,9 +212,9 @@
 	<!-- **************************************
 	MENUS TOP AND LEFT CONTAINER
 	******************************************* -->
-	<?php 
-	if(!isset($me)) 
-		$me=""; 
+	<?php
+	if(!isset($me))
+		$me="";
 	$collectionsType=array(
 		"Où sont les femmes",
 		"Passeur d'images",
@@ -231,8 +232,8 @@
 	);
 
 	$topList = Poi::getPoiByTagsAndLimit();
-	if (empty($topList)) $emptyTop=true; 
-	$this->renderPartial($layoutPath.'.menu.menuTop', array( "me" => $me , "topList" => $topList )); 
+	if (empty($topList)) $emptyTop=true;
+	$this->renderPartial($layoutPath.'.menu.menuTop', array( "me" => $me , "topList" => $topList ));
 	$this->renderPartial($layoutPath.'.menu.menuLeft', array("page" => "accueil", "myCity" => $city, "emptyTop"=>@$emptyTop)); ?>
 
 	<!-- **************************************
@@ -241,14 +242,14 @@
 	<div class="col-xs-12 no-padding no-margin my-main-container" <?php if (@$emptyTop) { ?> style="top:50px !important;" <?php } ?>>
 
 		<div class="footer-menu-left"></div>
-		
+
 		<!-- **************************************
 		MAIN ACTIVE SECTION
 		******************************************* -->
 		<div class="col-md-10 col-md-offset-2 col-sm-10 col-sm-offset-2 col-xs-12 main-col-search"></div>
-		
+
 		<!-- **************************************
-		REPERTOIRE 
+		REPERTOIRE
 		******************************************* -->
 		<div id="floopDrawerDirectory" class="floopDrawer"></div>
 
@@ -262,22 +263,22 @@
 	<!-- **************************************
 		NOTIFICATION PANELS
 		******************************************* -->
-	<?php  
-		if(isset(Yii::app()->session['userId'])) 
+	<?php
+		if(isset(Yii::app()->session['userId']))
 			$this->renderPartial($layoutPath.'notifications2');
-		
+
 		/* *****************************************
 		Active Content from the controller
 		******************************************* */
 		echo $content;
 
 		/* *************************************
-		END structure 
+		END structure
 		*******************************************/
 
 		  ?>
 		<!-- start: MAIN JAVASCRIPTS -->
-		
+
 		<?php
 		echo "<!-- start: MAIN JAVASCRIPTS -->";
 		echo "<!--[if lt IE 9]>";
@@ -292,31 +293,31 @@
 		//plugins shared by all themes
 		$cssAnsScriptFilesModule = array(
 			'/plugins/jquery-ui/jquery-ui-1.10.2.custom.min.js',
-			'/plugins/bootstrap/js/bootstrap.min.js' , 
+			'/plugins/bootstrap/js/bootstrap.min.js' ,
 			'/plugins/bootstrap/css/bootstrap.min.css',
-			'/plugins/bootstrap-fileupload/bootstrap-fileupload.min.js' , 
+			'/plugins/bootstrap-fileupload/bootstrap-fileupload.min.js' ,
 			'/plugins/bootstrap-fileupload/bootstrap-fileupload.min.css',
 			'/plugins/velocity/jquery.velocity.min.js',
-			'/plugins/ladda-bootstrap/dist/spin.min.js' , 
-			'/plugins/ladda-bootstrap/dist/ladda.min.js' , 
+			'/plugins/ladda-bootstrap/dist/spin.min.js' ,
+			'/plugins/ladda-bootstrap/dist/ladda.min.js' ,
 			'/plugins/ladda-bootstrap/dist/ladda.min.css',
 			'/plugins/ladda-bootstrap/dist/ladda-themeless.min.css',
-			'/plugins/iCheck/jquery.icheck.min.js' , 
+			'/plugins/iCheck/jquery.icheck.min.js' ,
 			'/plugins/iCheck/skins/all.css',
-			'/plugins/jquery.transit/jquery.transit.js' , 
-			'/plugins/TouchSwipe/jquery.touchSwipe.min.js' , 
-			'/plugins/bootbox/bootbox.min.js' , 
-			'/plugins/jquery-mockjax/jquery.mockjax.js' , 
-			'/plugins/blockUI/jquery.blockUI.js' , 
+			'/plugins/jquery.transit/jquery.transit.js' ,
+			'/plugins/TouchSwipe/jquery.touchSwipe.min.js' ,
+			'/plugins/bootbox/bootbox.min.js' ,
+			'/plugins/jquery-mockjax/jquery.mockjax.js' ,
+			'/plugins/blockUI/jquery.blockUI.js' ,
 
-			// '/plugins/toastr/toastr.js' , 
+			// '/plugins/toastr/toastr.js' ,
 			// '/plugins/toastr/toastr.min.css',
-			'/plugins/jquery-cookie/jquery.cookie.js' , 
-			'/plugins/jquery-cookieDirective/jquery.cookiesdirective.js' , 
-			'/plugins/jQuery-contextMenu/dist/jquery.contextMenu.min.js' , 
-			'/plugins/jQuery-contextMenu/dist/jquery.contextMenu.min.css' , 
+			'/plugins/jquery-cookie/jquery.cookie.js' ,
+			'/plugins/jquery-cookieDirective/jquery.cookiesdirective.js' ,
+			'/plugins/jQuery-contextMenu/dist/jquery.contextMenu.min.js' ,
+			'/plugins/jQuery-contextMenu/dist/jquery.contextMenu.min.css' ,
 			'/plugins/jQuery-contextMenu/dist/jquery.ui.position.min.js' ,
-			'/plugins/select2/select2.min.js' , 
+			'/plugins/select2/select2.min.js' ,
 			'/plugins/select2/select2.css',
 			'/plugins/moment/min/moment.min.js' ,
 			'/plugins/jquery-validation/dist/jquery.validate.min.js',
@@ -327,9 +328,9 @@
 			'/plugins/font-awesome/css/font-awesome.min.css',
 			'/plugins/font-awesome-custom/css/font-awesome.css',
 			'/plugins/jquery.dynForm.js',
-			'/plugins/jQuery-contextMenu/dist/jquery.contextMenu.min.js' , 
-			'/plugins/jQuery-contextMenu/dist/jquery.contextMenu.min.css' , 
-			'/plugins/jQuery-contextMenu/dist/jquery.ui.position.min.js' , 
+			'/plugins/jQuery-contextMenu/dist/jquery.contextMenu.min.js' ,
+			'/plugins/jQuery-contextMenu/dist/jquery.contextMenu.min.css' ,
+			'/plugins/jQuery-contextMenu/dist/jquery.ui.position.min.js' ,
 			'/js/api.js',
 
 		);
@@ -337,11 +338,14 @@
 
 		$cssAnsScriptFilesModule = array(
 			'/assets/js/cookie.js' ,
-			
+
+			'/assets/js/sly/jquery.easing.1.3.js',
+			'/assets/js/sly/sly.min.js',
+			'/assets/js/sly/sly.js',
 			'/assets/css/styles.css',
 			'/assets/css/styles-responsive.css',
 			'/assets/css/plugins.css',
-			
+
 			'/assets/css/search.css',
 			'/assets/css/default/directory.css',
 			'/assets/css/floopDrawerRight.css',
@@ -349,17 +353,17 @@
 		);
 		HtmlHelper::registerCssAndScriptsFiles($cssAnsScriptFilesModule, Yii::app()->theme->baseUrl);
 
-		
-		
+
+
 		//<!-- end: MAIN JAVASCRIPTS -->
 		//<!-- start: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
 		?>
 		<script type="text/javascript">
-	
+
 
 		var mapIconTop = {
 		    "default" : "fa-arrow-circle-right",
-		    "citoyen":"<?php echo Person::ICON ?>", 
+		    "citoyen":"<?php echo Person::ICON ?>",
 		    "NGO":"<?php echo Organization::ICON ?>",
 		    "LocalBusiness" :"<?php echo Organization::ICON_BIZ ?>",
 		    "Group" : "<?php echo Organization::ICON_GROUP ?>",
@@ -380,7 +384,7 @@
 		  };
 		var mapColorIconTop = {
 		    "default" : "dark",
-		    "citoyen":"yellow", 
+		    "citoyen":"yellow",
 		    "NGO":"green",
 		    "LocalBusiness" :"green",
 		    "Group" : "green",
@@ -422,11 +426,11 @@
 		var latCommunexion = 0;
 		var lngCommunexion = 0;
 
-		/* variables globales communexion */	
+		/* variables globales communexion */
 		var myContacts = <?php echo ($myFormContact != null) ? json_encode($myFormContact) : "null"; ?>;
 		var userConnected = <?php echo isset($me) ? json_encode($me) : "null"; ?>;
 
-		var proverbs = <?php echo json_encode(random_pic()) ?>;  
+		var proverbs = <?php echo json_encode(random_pic()) ?>;
 
 		var hideScrollTop = true;
 		var lastUrl = null;
@@ -447,8 +451,8 @@
 		var CoSigAllReadyLoad = true;
 
 		//console.warn("isMapEnd 1",isMapEnd);
-		jQuery(document).ready(function() { 
-			
+		jQuery(document).ready(function() {
+
 			themeObj.init();
 			/*if(typeof typeObj[key].dynForm.jsonSchema.properties.tags != "undefined"){
 				typeObj[key].dynForm.jsonSchema.properties.tags.data=networkTags;
@@ -463,20 +467,20 @@
 			$.each(rawOrganizerList, function(optKey, optVal) {
 				organizerList[optKey] = optVal.name;
 			});
-			
+
 			<?php if(isset(Yii::app()->session['userId']) && //et que le two_step est terminé
 					(!isset($me["two_steps_register"]) || $me["two_steps_register"] != true)){ ?>
-				
+
 				//if(location.hostname.indexOf("localhost") >= 0) path = "/ph/";
 				setCookies(location.pathname);
-				
+
 			<?php } ?>
 
 
 		  	if(inseeCommunexion != "" && cpCommunexion != ""){
 		  		$(".btn-menu2, .btn-menu3, .btn-menu4, .btn-menu9 ").show(400);
 		  	}
-		  	
+
 		  	$('#btn-toogle-map').click(function(e){ showMap();  	});
 		    $('.main-btn-toogle-map').click(function(e){ showMap(); });
 
@@ -500,10 +504,10 @@
 		    $(".btn-scope").mouseout(function(){
 		    	$(".btn-scope-niv-"+levelCommunexion).addClass("selected");
 		    });
-		    
+
 		    initNotifications();
 			initFloopDrawer();
-		    
+
 		    $(window).resize(function(){
 		      resizeInterface();
 		    });
@@ -520,8 +524,8 @@
 		   // toogleCommunexion();
 
 
-			//manages the back button state 
-			//every url change (loadByHash) is pushed into history.pushState 
+			//manages the back button state
+			//every url change (loadByHash) is pushed into history.pushState
 			//onclick back btn popstate is launched
 			//
 		    $(window).bind("popstate", function(e) {
@@ -533,7 +537,7 @@
 			        //console.warn("poped state",location.hash);
 			        //alert("popstate");
 			        loadByHash(location.hash,true);
-			    } 
+			    }
 			    allReadyLoad = false;
 		      }
 
@@ -543,7 +547,7 @@
 
 			//console.log("start timeout MAIN MAP LOOOOOL");
 			//$("#btn-toogle-map").hide();
-			
+
 
 
 		    //console.log("hash", location.hash);
@@ -556,14 +560,14 @@
 			if(userConnected != null && userConnected != "" && typeof userConnected != "undefined" && !location.hash){
 				loadByHash("#person.detail.id."+userId);
 				return;
-			} 
+			}
 			else{ //si l'utilisateur est déjà passé par le two_step_register
 		 		if(/*location.hash != "#default.live" &&*/ location.hash != "#" && location.hash != ""){
 
 					loadByHash(location.hash);
 					return;
 				}
-				else{ 
+				else{
 					//console.log("userConnected", userConnected);
 					if(userConnected != null && userId != null  && userId != "" && typeof userId != "undefined")
 						loadByHash("#default.home");//news.index.type.citoyens.id."+userId);
@@ -635,7 +639,7 @@
 		            "inputType" : "hidden"
 				};
 
-				//remove image 
+				//remove image
 				delete typeObjPoi.properties.image;
 				delete typeObjPoi.afterSave;
 				delete typeObjPoi.beforeBuild;
@@ -646,7 +650,7 @@
 		            "inputType" : "array",
 		            "value" : [],
 		            init:function(){
-			            getMediaFromUrlContent(".addmultifield0", ".resultGetUrl0",0);			            
+			            getMediaFromUrlContent(".addmultifield0", ".resultGetUrl0",0);
 		            }
 		        };
 		        typeObjPoi.properties.urls=poiFormUrls;
@@ -681,7 +685,7 @@
             }
 		}
 
-		
+
 		</script>
 
 		<!-- end: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
