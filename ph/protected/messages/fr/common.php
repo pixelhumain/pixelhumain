081a51041a2b6a16564a5babe4bfc95690ae1361
Bouboule <clement.damiens@gmail.com>
2017-06-22 18:13:20 +0200

    trad fémin not specified

--- ph/protected/messages/fr/common.php
+++ ph/protected/messages/fr/common.php
@@ -852,7 +852,7 @@ merci de renseigner votre code postal dans l'addresse de votre profil.",
 	"About" => "A propos",
 
 	"Update Params" => "Mettre à jour vos paramètres",
-	"Not specified" => "Non renseigné",
+	"Not specified" => "Non renseigné(e)",
 	"Update general information" => "Mettre à jour les informations générales",
 	"Update description" => "Mettre à jour la description",
 	"Update network" => "Mettre à jour les réseaux sociaux",
