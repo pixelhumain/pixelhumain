80d3d8b1c14e25536feaeff86ea946c2b5946788
Oceatoon <oceatoon@gmail.com>
2017-07-04 17:59:06 +0400

    debug img

--- ph/plugins/jquery.dynForm.js
+++ ph/plugins/jquery.dynForm.js
@@ -1018,8 +1018,9 @@ onSave: (optional) overloads the generic saveProcess
 			$(".fine-uploader-manual-trigger").fineUploader({
 	            template: 'qq-template-gallery',//'qq-template-manual-trigger',
 	            request: {
-	                endpoint: baseUrl+"/"+moduleId+"/document/uploadSave/dir/"+uploadObj.folder+"/folder/"+uploadObj.type+"/ownerId/"+uploadObj.id+"/input/qqfile/contentKey/"+uploadObj.contentKey
-	                //params : {contentKey:contentKey}
+	                endpoint: baseUrl+"/"+moduleId+"/document/uploadSave/dir/"+uploadObj.folder+"/folder/"+uploadObj.type+"/ownerId/"+uploadObj.id+"/input/qqfile/contentKey/"+uploadObj.contentKey,
+	                //endpoint: baseUrl+"/"+moduleId+"/document/uploadSave/dir/"+uploadObj.folder+"/folder/"+uploadObj.type+"/ownerId/"+uploadObj.id+"/input/qqfile/contentKey/"+uploadObj.contentKey 
+	                //params : {id:uploadObj.id}
 	            },
 	            validation: {
 	                allowedExtensions: (initValues.filetypes) ? initValues.filetypes : ['jpeg', 'jpg', 'gif', 'png'],
@@ -1032,6 +1033,7 @@ onSave: (optional) overloads the generic saveProcess
 	            callbacks: {
 	            	//when a img is selected
 				    onSubmit: function(id, fileName) {
+				    	//alert(" > onSubmit : "+ uploadObj.id +" :: "+ uploadObj.type);
 				      if(initValues.showUploadBtn)
 				      	$('#trigger-upload').removeClass("hide");
 				    },
@@ -1047,7 +1049,11 @@ onSave: (optional) overloads the generic saveProcess
 				    },
 				    //launched on upload
 				    onProgress: function(id, fileName, uploadedBytes,totalBytes) {
-				      alert("progress"+ uploadObj.id +" :: "+ uploadObj.type);
+				    	var ex = $('.fine-uploader-manual-trigger').fineUploader('getEndpoint');
+				    	console.log('getEndpoint',ex);
+				    	console.log('getInProgress',$('.fine-uploader-manual-trigger').fineUploader('getInProgress'));
+				      alert("progress > "+" :: "+ uploadObj.id +" :: "+ uploadObj.type);
+				      //alert("progress >> "+ " :: ");
 				    },
 				    //when every img finish upload process whatever the status
 				    onComplete: function(id, fileName,responseJSON,xhr) {
@@ -1058,7 +1064,7 @@ onSave: (optional) overloads the generic saveProcess
 				    },
 				    //when all upload is complete whatever the result
 				    onAllComplete: function(succeeded, failed) {
-				      toastr.info( "onAllComplete Fichiers bien chargés !!"+ uploadObj.id +" :: "+ uploadObj.type );
+				      toastr.info( "onAllComplete Fichiers bien chargés !!");//+ uploadObj.id +" :: "+ uploadObj.type );
 
 				      if( jQuery.isFunction(initValues.afterUploadComplete) )initValues.afterUploadComplete();
 				      uploadObj.gotoUrl = null;
