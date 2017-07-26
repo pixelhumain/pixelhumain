6f47bcca352088dd4386da1cb0217024a0950030
Bouboule <clement.damiens@gmail.com>
2017-07-26 19:04:43 +0200

    trad directory y otro

---
+++ ph/protected/messages/fr/category.php
@@ -0,0 +1,45 @@
+<?php
+	return array(
+		//Filliaire
+		"Commons"=>"Communs",
+		"Agriculture | Fooding"=>"Agriculture | Alimentation",
+		"Health"=>"Santé",
+		"Trashes"=>"Déchets",
+		"Planning | Transport | Construction"=>"Aménagement | Transport | Construction",
+		"Education | Childhood"=>"Éducation | Petite-enfance",
+		"Citizenship"=>"Citoyenneté",
+		"Economy"=>"Economie",
+		"Energy | Climate"=>"Energie | Climat",
+		"Culture | Animation"=>"Culture | Animation",
+		"Biodiversity"=>"Biodiversité",
+		"TIC | Numeric | Internet"=>"TIC | Numérique | Internet",
+		//Classifieds
+        "Share"=>"Partager", 
+        "To share"=>"À partager"
+        "Give"=>"Donner",   
+        "To give"=>"À donner",        
+        "Sale"=>"Vendre",  
+        "For sale"=>"À vendre",            
+        "Rent"=>"Louer",   
+        "For rent"=>"À louer",
+        "Need"=>"Besoin", 
+        "Looking for"=>"À la recherche",   
+        "Jobs"=>"Emplois", 
+        "Offers"=>"Offres d'emplois",  
+        //Types event
+		"Contest" => "Concours",
+	    "Festival" => "Festival",
+	    "Market" => "Marché",
+	    "Others" => "Autres",
+	    "Concert" => "Concert",
+	    "Exposition" => "Exposition",
+	    "Get together" => "Rencontre",
+	    "Meeting" => "Réunion",
+	    "Competition" => "Compétition",
+	    "Course" => "Formation",
+	    "Workshop" => "Atelier",
+	    "Conference" => "Conférence",
+	    "Debate" => "Débat",
+	
+	);
+?>
