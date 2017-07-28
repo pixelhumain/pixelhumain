f8d5e5090e0c3bed38059068dfddd35dd84bddec
Kgneo <tristan.goguet@gmail.com>
2017-07-28 15:26:46 +1100

    up main menu

--- ph/themes/CO2/views/layouts/menus/moduleMenu.php
+++ ph/themes/CO2/views/layouts/menus/moduleMenu.php
@@ -9,14 +9,13 @@
                 </a>    
 <?php       }
         }  
-        
 ?>
-<?php if(Yii::app()->params["CO2DomainName"] != "kgougle" && Yii::app()->params["CO2DomainName"] != "BCH"){ ?>
-<a href="#default.view.page.links" class="lbhp text-red link-submenu-header margin-right-25" >
-    <i class="fa fa-life-ring"></i> Aide
-</a>
 
-<a href="default/view/page/kickerFunding/dir/docs|panels" class="lbhp text-red link-submenu-header margin-right-25" >
-    <i class="fa fa-heart"></i> Contribuer
-</a>
+<?php if(Yii::app()->params["CO2DomainName"] != "kgougle" && Yii::app()->params["CO2DomainName"] != "BCH"){ ?>
+    <a href="#default.view.page.links" class="lbhp text-red link-submenu-header margin-right-25" >
+        <i class="fa fa-life-ring"></i> Aide
+    </a>
+    <a href="default/view/page/kickerFunding/dir/docs|panels" class="lbhp text-red link-submenu-header margin-right-25" >
+        <i class="fa fa-heart"></i> Contribuer
+    </a>
 <?php } ?>
