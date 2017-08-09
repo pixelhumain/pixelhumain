f00e9cc060fe4078856039d58083bea328924952
Kgneo <tristan.goguet@gmail.com>
2017-08-10 00:30:25 +1100

    start refactor

---
+++ ph/protected/messages/fr/cooperation.php
@@ -0,0 +1,41 @@
+<?php
+
+return array(
+
+	//GLOBAL INTERFACE 
+	"Resolutions"=>"Résolutions",
+	"Actions"=>"Actions",
+	"Proposals"=>"Propositions",
+	"Rooms"=>"Espaces CO",
+
+	"resolution"=>"Résolutions",
+	"action"=>"Actions",
+	"proposal"=>"Propositions",
+	"room"=>"Espaces CO",
+
+	"Create action" => "Créer une action",
+	"Add action" => "Ajouter une action",
+	"My actions" => "Mes actions",
+	"To do" => "À faire",
+	"Done" => "Terminées",
+	"Archived" => "Archivées",
+
+	"Create proposal" => "Créer une proposition",
+	"Add proposal" => "Ajouter une proposition",
+	"My proposals" => "Mes propositions",
+	"Amendable" => "Amendables",
+	"To vote" => "À voter",
+	"Closed" => "Fermées",
+
+	"mine" => "À moi",
+	"amendable" => "Amendables",
+	"tovote" => "À voter",
+	"todo" => "À faire",
+	"archived" => "archivée",
+	"closed" => "Fermées",
+	"done" => "terminée",
+
+	"Create room" => "Créer un espace",
+	
+	"Topic" => "Sujet",
+);
