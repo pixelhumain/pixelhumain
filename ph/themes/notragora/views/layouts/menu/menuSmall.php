2bdf35856ac26fe78b5bf85e696357c83294f9d6
LassalleRemi <remil.lassalle@gmail.com>
2017-08-24 10:19:48 +0200

    recaptcha et bouton mes groupes

--- ph/themes/notragora/views/layouts/menu/menuSmall.php
+++ ph/themes/notragora/views/layouts/menu/menuSmall.php
@@ -1,7 +1,7 @@
 
 
-<?php  
-HtmlHelper::registerCssAndScriptsFiles(array('/assets/css/menus/menuSmall.css'), Yii::app()->theme->baseUrl); 
+<?php
+HtmlHelper::registerCssAndScriptsFiles(array('/assets/css/menus/menuSmall.css'), Yii::app()->theme->baseUrl);
 
 if (isset(Yii::app()->session['userId']) && !empty($me)) {
           $profilMediumImageUrl = Element::getImgProfil($me, "profilMediumImageUrl", $this->module->assetsUrl);
@@ -18,41 +18,47 @@ if (isset(Yii::app()->session['userId']) && !empty($me)) {
 					<i class="fa fa-sign-in"></i>
 					</br>Se Connecter
 				</a>
-			</div> 
+			</div>
 			<div class="col-md-12 col-sm-12 col-xs-6">
 				<a class="btn bg-grey" href="javascript:;" onclick="showPanel('box-register');$.unblockUI();">
 					<i class="fa fa-plus-circle"></i>
 					</br>S'inscrire
 				</a>
-			</div> 
-		</div> 
+			</div>
+		</div>
 		<?php }  else { ?>
-		<div class="col-md-3 col-sm-3 col-xs-12 center margin-top-15 margin-bottom-5">
+		<div class="col-md-3 col-sm-3 col-xs-12 center margin-top-15 margin-bottom-5" id="menusmall">
 			<div class="col-md-12 col-sm-12 no-padding" id="menu-my-profil">
 				<span class="text-white label text-bold" style="font-size:16px !important;"><?php echo $me["name"]; ?></span>
 				<div id="img-my-profil">
 					<a class="no-border lbh" href="#person.detail.id.<?php echo Yii::app()->session['userId']?>" >
-						<img class="img-responsive thumbnail" id="menu-small-thumb-profil" 
+						<img class="img-responsive thumbnail" id="menu-small-thumb-profil"
 						src="<?php echo $profilMediumImageUrl; ?>" alt="image">
 						<span id="menu-my-profil-text">Mon profil</span>
 					</a>
 				</div>
 			</div>
 
+      <div class="col-xs-12 center no-padding">
+        <a class="btn bg-green padding-5" href="#person.detail.id.<?php echo Yii::app()->session['userId']?>?view=directory">
+            <i class="fa fa-group"></i>
+            <br>Mes groupes
+        </a>
+      </div>
 
 		    <?php if(isset($me)) if(Role::isSuperAdmin($me['roles'])){?>
 
 			<div class="col-xs-12 center no-padding">
 			    <a class="btn bg-dark-red lbh" href="#admin.index">
-			        <i class="fa fa-user-secret" style="font-size: 1em!important;"></i> 
+			        <i class="fa fa-user-secret" style="font-size: 1em!important;"></i>
 			        Admin
 			    </a>
 		    </div>
-			<?php } ?>	
+			<?php } ?>
 			<div class="col-xs-12 center no-padding">
 				<a class="btn bg-dark visible-xs padding-5" href="javascript:$('.btn-menu-notif').trigger('click');$.unblockUI();">
-			        <i class="fa fa-bell" style="font-size: 1em!important;"></i> 
-			        <span class="notifications-count topbar-badge badge badge-danger animated bounceIn" 
+			        <i class="fa fa-bell" style="font-size: 1em!important;"></i>
+			        <span class="notifications-count topbar-badge badge badge-danger animated bounceIn"
 		        		  style="position:relative; top:-2px; left:unset;">
 		        		<?php count($this->notifications); ?>
 			        </span>
@@ -62,28 +68,28 @@ if (isset(Yii::app()->session['userId']) && !empty($me)) {
 			<?php /* ?>
 		    <div class="col-xs-12 center no-padding">
 			    <a class="btn bg-dark lbh padding-5" href="#news.index.type.citoyens.id.<?php echo Yii::app()->session['userId'] ?>.viewer.<?php echo Yii::app()->session['userId']?>"  >
-			        <i class="fa fa-newspaper-o" style="font-size: 1em!important;"></i> 
+			        <i class="fa fa-newspaper-o" style="font-size: 1em!important;"></i>
 			        Mon journal
 			    </a>
 		    </div>
 		    <div class="col-xs-12 center no-padding">
 			    <a class="btn bg-dark lbh padding-5" href="#person.directory.id.<?php echo Yii::app()->session['userId'] ?>?tpl=directory2">
-			        <i class="fa fa-group" style="font-size: 1em!important;"></i> 
+			        <i class="fa fa-group" style="font-size: 1em!important;"></i>
 			        Mon répertoire
 			    </a>
 		    </div>
 		    <?php */ ?>
 		    <div class="col-xs-12 center no-padding">
-			    <a class="btn bg-red padding-5" 
+			    <a class="btn bg-red padding-5"
 			    	href="<?php echo Yii::app()->createUrl('/'.$this->module->id.'/person/logout'); ?>">
 			        <i class="fa fa-sign-out"></i>
 			        <br><?php echo Yii::t("person","Sign out"); ?>
 			    </a>
 		    </div>
-			   
+
 		</div>
-		<?php } ?>	
-		
+		<?php } ?>
+
 
 	  	<div class="col-md-9 col-sm-9 col-xs-12 no-padding">
 			<div class="col-md-12 col-sm-12 padding-15">
@@ -91,8 +97,8 @@ if (isset(Yii::app()->session['userId']) && !empty($me)) {
 					<div class="col-md-12 col-sm-12  col-xs-12 no-padding">
 						<hr style="border-top: 1px solid transparent; margin:7px;">
 						<h2 class="homestead text-white">
-							<i class="fa fa-plus-circle"></i> Ajouter 
-							<i class="fa fa-angle-down"></i> 
+							<i class="fa fa-plus-circle"></i> Ajouter
+							<i class="fa fa-angle-down"></i>
 						</h2>
 					</div>
 					<?php /* ?>
@@ -133,32 +139,32 @@ if (isset(Yii::app()->session['userId']) && !empty($me)) {
 					<hr style="border-top: 1px solid transparent; margin:7px;">
 					<h2 class="homestead text-white">
 						Comprendre<?php if(isset(Yii::app()->session['userId'])) echo " et aider"; ?>
-						<i class="fa fa-angle-down"></i> 
+						<i class="fa fa-angle-down"></i>
 					</h2>
 				</div>
 				<?php if(isset(Yii::app()->session['userId'])) $colDoc="6"; else $colDoc="6"; ?>
 				<div class="col-xs-<?php echo $colDoc;?> col-sm-<?php echo $colDoc;?> col-md-<?php echo $colDoc;?> center padding-5">
-					<a href="#default.view.page.index.dir.docs" 
-						class="btn bg-grey lbh menu-button btn-menu btn-menu-notif tooltips text-white" 
+					<a href="#default.view.page.index.dir.docs"
+						class="btn bg-grey lbh menu-button btn-menu btn-menu-notif tooltips text-white"
 			            data-toggle="tooltip" data-placement="left" title="Documentation">
-				        <i class="fa fa-file"></i> 
+				        <i class="fa fa-file"></i>
 				        <br/>Documentation
 				    </a>
 			    </div>
 			   <?php //if(isset(Yii::app()->session['userId'])){ ?>
 			    <div class="col-xs-6 col-sm-6 col-md-6 center padding-5">
-					<a  href="#news.index.type.pixels" 
-						class="btn bg-grey lbh menu-button btn-menu btn-menu-notif tooltips text-white" 
+					<a  href="#news.index.type.pixels"
+						class="btn bg-grey lbh menu-button btn-menu btn-menu-notif tooltips text-white"
 			            data-toggle="tooltip" data-placement="left" title="Signaler un bug">
-				        <i class="fa fa-bullhorn"></i> 
+				        <i class="fa fa-bullhorn"></i>
 				        <br/>Signaler un bug
 				    </a>
 			    </div>
 			    <?php //} ?>
 
-			    
+
 			</div>
-			
+
 	   	</div>
 	</div>
 </div>
