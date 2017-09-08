abc447a56ea5a76c6a3257b6390f2d98bee6f503
Bouboule <clement.damiens@gmail.com>
2017-09-08 16:58:23 +0200

    cochon

---
+++ ph/protected/messages/fr/category.php
@@ -0,0 +1,15 @@
+<?php
+
+	return array(
+
+//ROLES LINK 
+        "Financier"=>"Financeur", 
+        "Partner"=>"Partenaire", 
+        "Sponsor"=>"Sponsor", 
+        "Organizor"=>"Organisateur", 
+        "President"=>"Président", 
+        "Director"=>"Directeur", 
+        "Speaker"=>"Conférencier", 
+        "Intervener"=>"Intervenant" 
+        );
+        ?>
