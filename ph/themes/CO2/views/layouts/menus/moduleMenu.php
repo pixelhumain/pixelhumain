e20e97bcd78cab6b4809d74806cbdb8aa7682dc5
Kgneo <tristan.goguet@gmail.com>
2017-09-13 11:30:00 +1100

    image background header

--- ph/themes/CO2/views/layouts/menus/moduleMenu.php
+++ ph/themes/CO2/views/layouts/menus/moduleMenu.php
@@ -1,7 +1,7 @@
 <?php //if($subdomainName != "web" && $subdomainName != "referencement") 
         foreach ($params["pages"] as $key => $value) {
             if(@$value["inMenu"]==true && @$value["open"]==true){ ?>
-                <a  class="lbh text-red link-submenu-header margin-right-25 
+                <a  class="lbh text-dark link-submenu-header margin-right-15 
                             <?php if("#".$subdomain == $key) echo "active"; ?>" 
                     href="<?php echo $key; ?>">
                     <i class="fa fa-<?php echo $value["icon"]; ?>"></i>
@@ -12,11 +12,11 @@
 ?>
 
 <?php if(Yii::app()->params["CO2DomainName"] != "kgougle" && Yii::app()->params["CO2DomainName"] != "BCH"){ ?>
-<a href="#default.view.page.links" class="lbhp text-red link-submenu-header margin-right-25" >
-    <i class="fa fa-life-ring"></i> <?php echo Yii::t("common","Help") ?>
+<a href="#default.view.page.links" class="lbhp text-dark link-submenu-header margin-right-15" >
+    <i class="fa fa-life-ring"></i> <span class=""><?php echo Yii::t("common","Help") ?></span>
 </a>
 
-<a href="default/view/page/kickerFunding/dir/docs|panels" class="lbhp text-red link-submenu-header margin-right-25" >
-    <i class="fa fa-heart"></i> <?php echo Yii::t("common", "Contribute") ?>
+<a href="default/view/page/kickerFunding/dir/docs|panels" class="lbhp text-dark link-submenu-header margin-right-15" >
+    <i class="fa fa-heart"></i> <span class=""><?php echo Yii::t("common", "Contribute") ?></span>
 </a>
 <?php } ?>
