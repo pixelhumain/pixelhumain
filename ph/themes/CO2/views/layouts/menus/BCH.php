741e818d190dd7b3658b703fb17fbbb4a2589dda
Bouboule <clement.damiens@gmail.com>
2017-09-19 00:17:29 +0200

    up menu BCH

--- ph/themes/CO2/views/layouts/menus/BCH.php
+++ ph/themes/CO2/views/layouts/menus/BCH.php
@@ -1,5 +1,58 @@
 <!-- Navigation -->
-<nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
+<nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom no-padding">
+    <div class="container bg-black">
+                
+        <ul class="nav navbar-nav navbar-right">
+                    <li>
+                    <a class="font-montserrat text-white padding-10" id="btn-open-search-bar">
+                        <span><i class="fa fa-shopping-cart"></i></span>
+                    </a>
+                    </li>
+                    <?php if (!@Yii::app()->session["userId"]){ ?>
+                        <li>
+                        <a class="letter-orange font-montserrat btn-menu-connect padding-10" 
+                            data-toggle="modal" data-target="#modalLogin">
+                            <span><i class="fa fa-2x fa-user-circle"></i></span>
+                        </a>
+                        </li>
+                    <?php }else{ 
+                        $profilThumbImageUrl = Element::getImgProfil($me, "profilThumbImageUrl", $this->module->assetsUrl); ?>
+                        <li>
+                        <a class="letter-orange font-montserrat padding-10"> 
+                                <img class="img-circle" id="menu-thumb-profil" style="margin-top:-5px;" 
+                                     width="25" height="25" src="<?php echo $profilThumbImageUrl; ?>" alt="image" >
+                        </a>
+                        <div class="dropdown pull-right" id="dropdown-user">
+                            <div class="dropdown-main-menu">
+                                <ul class="dropdown-menu arrow_box">
+                                    <li class="text-left">
+                                        <a href="<?php echo Yii::app()->createUrl('/co2/person/logout'); ?>" 
+                                            class="bg-white letter-red logout">
+                                            <i class="fa fa-sign-out"></i> <?php echo Yii::t("common", "Log Out") ; ?>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <li>
+                    <?php } ?>
+                      <li class="dropdown">
+                            <a href="#" class="dropdown-toggle padding-10 text-white" data-toggle="dropdown" role="button" style="height: 35px;">
+                        <img src="<?php echo Yii::app()->getRequest()->getBaseUrl(true); ?>/images/flags/<?php echo Yii::app()->language ?>.png" width="22"/> <span class="caret"></span></a>
+                            <ul class="dropdown-menu arrow_box" role="menu" style="position: absolute !important;
+                                top: 45px;
+                                right: -65px;
+                                left: inherit;
+                                background-color: white;
+                                border: 1px solid transparent;
+                                -webkit-box-shadow: 0 6px 12px rgba(0,0,0,.175);
+                                box-shadow: 0 6px 12px rgba(0,0,0,.175);">
+                                <li><a href="javascript:;" onclick="setLanguage('en')"><img src="<?php echo Yii::app()->getRequest()->getBaseUrl(true); ?>/images/flags/en.png" width="25"/> <?php echo Yii::t("common","English") ?></a></li>
+                                <li><a href="javascript:;" onclick="setLanguage('fr')"><img src="<?php echo Yii::app()->getRequest()->getBaseUrl(true); ?>/images/flags/fr.png" width="25"/> <?php echo Yii::t("common","French") ?></a></li>
+                            </ul>
+                        </li>
+            </ul>
+    </div>
     <div class="container">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header pull-left">
@@ -7,7 +60,7 @@
             <a href="#" class="btn btn-link menu-btn-back-category pull-left no-padding lbh" 
                 <?php //if( $subdomain != "welcome" ) { echo 'data-target="#modalMainMenu" data-toggle="modal"' } ?>
             >
-                <img src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/img/LOGOS/<?php echo Yii::app()->params["CO2DomainName"]; ?>/logo-min.png" 
+                <img src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/img/LOGOS/<?php echo Yii::app()->params["CO2DomainName"]; ?>/logo.png" 
                      class="logo-menutop pull-left" height=30>
             </a>
             <span class="hidden-xs skills font-montserrat"><?php echo $mainTitle; ?></span>
@@ -28,34 +81,26 @@
             <ul class="nav navbar-nav navbar-right">
                 <li class="pull-right">
                     <button class="letter-orange font-montserrat" id="btn-open-search-bar">
-                        <span><i class="fa fa-2x fa-search"></i></span>
+                        <span>Our circuits</span>
                     </button>
-                    <?php if (!@Yii::app()->session["userId"]){ ?>
-                        <button class="letter-orange font-montserrat btn-menu-connect" 
-                            data-toggle="modal" data-target="#modalLogin">
-                            <span><i class="fa fa-2x fa-user-circle"></i></span>
-                        </button>
-                    <?php }else{ 
-                        $profilThumbImageUrl = Element::getImgProfil($me, "profilThumbImageUrl", $this->module->assetsUrl); ?>
-                        <button class="letter-orange font-montserrat btn-show-mainmenu"> 
-                                <img class="img-circle" id="menu-thumb-profil" style="margin-top:-10px;" 
-                                     width="25" height="25" src="<?php echo $profilThumbImageUrl; ?>" alt="image" >
-                        </button>
-                        <div class="dropdown pull-right" id="dropdown-user">
-                            <div class="dropdown-main-menu">
-                                <ul class="dropdown-menu arrow_box">
-                                    <li class="text-left">
-                                        <a href="<?php echo Yii::app()->createUrl('/co2/person/logout'); ?>" 
-                                            class="bg-white letter-red logout">
-                                            <i class="fa fa-sign-out"></i> <?php echo Yii::t("common", "Log Out") ; ?>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    <?php } ?>
-                    <button class="letter-orange font-montserrat">
-                        <span><i class="fa fa-2x fa-flag"></i> FR</span>
+
+                    <button class="letter-orange font-montserrat" id="btn-open-search-bar">
+                        <span>On the map</span>
+                    </button>
+
+                    <button class="letter-orange font-montserrat" id="btn-open-search-bar">
+                        <span>Agenda</span>
+                    </button>
+
+                    <button class="letter-orange font-montserrat" id="btn-open-search-bar">
+                        <span>Store</span>
+                    </button>
+                    <button class="letter-orange font-montserrat" id="btn-open-search-bar">
+                        <span>Community</span>
+                    </button>
+                    
+                    <button class="letter-orange font-montserrat" id="btn-open-search-bar">
+                        <span>Contribute</span>
                     </button>
                 </li>
             </ul>
