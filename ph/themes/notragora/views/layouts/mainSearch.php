fbe7c681106aba9ab8c99a0643bb62c7eb491a63
SimonSarazin <simon.sarazin@gmail.com>
2017-09-20 13:05:34 +0200

    Added Analytics

--- ph/themes/notragora/views/layouts/mainSearch.php
+++ ph/themes/notragora/views/layouts/mainSearch.php
@@ -690,6 +690,17 @@
 
 		</script>
 
+		<!-- Global Site Tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-25531131-1"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments)};
+		  gtag('js', new Date());
+
+		  gtag('config', 'UA-25531131-1');
+		</script>
+		
+		
 		<!-- end: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
 	</body>
 	<!-- end: BODY -->
