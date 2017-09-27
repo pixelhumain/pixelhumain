d795fbbb386876d52d1e1a79409f4b6fa9291a94
codev <raphaelriviere06@gmail.com>
2017-09-27 11:34:26 +0400

    Modif versionning

--- ph/protected/config/paramsconfig.php
+++ ph/protected/config/paramsconfig.php
@@ -88,6 +88,7 @@ $params = array(
 
     'CO2DomainName' => 'CO2', //used by CO2 theme, param header / css / js specific for each website using CO2 theme
     "theme"=> "CO2",
+    "version"=> "2.1.0",
 
     //rocketchat params
     'rocketchatEnabled' => true,
