06ba242fdfc7345dd7353265f986040a888a1d09
codev <raphaelriviere06@gmail.com>
2017-09-28 11:53:30 +0400

    Correction

--- ph/protected/messages/es/document.php
+++ ph/protected/messages/es/document.php
@@ -2,7 +2,7 @@
 
 	return array(
 		"only" => "Seul",
-		"Document has been deleting" => "Le document a bien été supprimé",
+		"Document has been deleted" => "Le document a bien été supprimé",
 		"files are allowed!" => "sont autorisés",
 		"Error! Wrong HTTP method!" => "Erreur! Mauvaise methode HTTP",
 		"Something went wrong with your upload!" => "Le chargement du document ne s'est pas deroulé correctement",
