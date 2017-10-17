20bdd638dd1b09f5365ca49c0626631753e0eb78
Bouboule <clement.damiens@gmail.com>
2017-10-17 18:56:06 +0200

    add countquantity

--- ph/themes/terla/views/layouts/initJs.php
+++ ph/themes/terla/views/layouts/initJs.php
@@ -37,7 +37,7 @@
     var ressource = <?php echo json_encode( CO2::getContextList("ressource") ) ?>;
     var poi = <?php echo json_encode( CO2::getContextList("poi") ) ?>;
     var roomList = <?php echo json_encode( CO2::getContextList("room") ) ?>;
-    var shoppingCart = {};
+    var shoppingCart = {countQuantity:0};
     //var classifiedSubTypes = <?php //echo json_encode( Classified::$classifiedSubTypes ) ?>;
     var urlTypes = <?php asort(Element::$urlTypes); echo json_encode(Element::$urlTypes) ?>;
     
