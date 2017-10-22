0e248dac685e18198c94045d8e18698ec9fddbcf
scoid <scoid@users.noreply.github.com>
2017-10-22 12:04:10 +0200

    Update activityList.php
    -> traduction allemande, German translation

--- ph/protected/messages/de/activityList.php
+++ ph/protected/messages/de/activityList.php
@@ -1,7 +1,7 @@
 <?php
 
 	return array(
-		"There is no activity for the moment.<br/>Edit...<br/>To improve the commons informations<br/>To give a better overview of territories' actors and activities<br/>And to liberate your knowledge" => "Zur Zeit gibt es keine Aktivität.<br/>Bearbeiten...<br/>Um gemeinsame Inhalte zu verbessern,<br/>Um einen besseren Überblick über Akteure und Aktivitäten in der Region zu erhalten<br/>Und um Ihr Wissen zu teilen !",
+		"There is no activity for the moment.<br/>Edit...<br/>To improve the commons informations<br/>To give a better overview of territories' actors and activities<br/>And to liberate your knowledge" => "Zur Zeit gibt es keine Aktivität.<br/>Bearbeiten...<br/>Um Inhalte der Allmende zu verbessern,<br/>Um einen besseren Überblick über Akteure und Aktivitäten in der Region zu erhalten<br/>Und um Ihr Wissen zu teilen !",
 		"See modifications" => "Änderungen ansehen"
 	)
 
