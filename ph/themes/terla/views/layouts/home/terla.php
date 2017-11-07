9d13ad57b28c09906c44410e777d7015edc2547d
Kgneo <tristan.goguet@gmail.com>
2017-11-07 13:04:19 +1100

    page service design

--- ph/themes/terla/views/layouts/home/terla.php
+++ ph/themes/terla/views/layouts/home/terla.php
@@ -32,7 +32,7 @@
 		    <div class="item active">
 		      <img src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/img/reunion/reunion4.jpg" alt="Reunion 1">
 		      <div class="carousel-caption">
-		        <h3>BIENVENUE CHEZ L'HABITANT</h3>
+		        <h3><?php echo Yii::t("terla", "TERLA"); ?></h3>
 		        <p>Etiam ultricies pulvinar porttitor, duis ac purus libero. Phasellus accumsan porta libero. 
 				</p>
 		      </div>
@@ -41,8 +41,8 @@
 		    <div class="item">
 		      <img src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/img/reunion/reunion5.jpg" alt="Reunion 1">
 		      <div class="carousel-caption">
-		        <h3>BIENVENUE CHEZ L'HABITANT</h3>
-		        <p>L’objectif de ce projet est de promouvoir la culture réunionnaise en passant par des habitant, ou des prestataire à taille humaine, qui ont une légitimité pour la représenter. Des acteurs qui vont garantir la véracité et l’authenticité de notre culture et favoriser son développement. 
+		        <h3><?php echo Yii::t("terla", "TERLA"); ?></h3>
+		        <p>Etiam ultricies pulvinar porttitor, duis ac purus libero. Phasellus accumsan porta libero. 
 				</p>
 		      </div>
 		    </div>
@@ -52,24 +52,24 @@
 		  <!-- Left and right controls -->
 		  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
 		    <span class="glyphicon glyphicon-chevron-left"></span>
-		    <span class="sr-only">Précédent</span>
+		    <span class="sr-only"><?php echo Yii::t("common", "back"); ?></span>
 		  </a>
 		  <a class="right carousel-control" href="#myCarousel" data-slide="next">
 		    <span class="glyphicon glyphicon-chevron-right"></span>
-		    <span class="sr-only">Suivant</span>
+		    <span class="sr-only"><?php echo Yii::t("common", "next"); ?></span>
 		  </a>
 		</div>
 	</div>
 	<div class="row col-md-10 col-md-offset-1 margin-bottom-50 margin-top-25 blockHome1">
 		<div class="row col-md-10 col-md-offset-1 bg-lightgray">
-			<h2 class="block letter-lightgray">Some of our top trip</h2>
+			<h2 class="block letter-lightgray"><?php echo Yii::t("terla", "Some of our top services"); ?></h2>
 			<div class="row col-md-10">	
 				<span>
 					Suspendisse feugiat dui magna, vel molestie mauris porttitor quis. Vivamus et enim felis tincidunt nisi pulvinar mi.
 				</span>
 			</div>
 			<div class="row col-md-2">
-				<a href="javascript:;" class="btn bg-orange"><b>VIEW MORE</b></a>
+				<a href="javascript:;" class="btn bg-orange"><b><?php echo Yii::t("terla", "VIEW MORE"); ?></b></a>
 			</div>
 		</div>
 	</div>
@@ -86,7 +86,7 @@
 		    <div id="presentation-home-slogan" class="row col-md-12  content-img">
 				<img 	src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/img/reunion/reunion4.jpg" 
 		         		alt="Generic placeholder thumbnail" height=250>
-		        <h3 class="text-white title">What's client say</h3>
+		        <h3 class="text-white title"><?php echo Yii::t("terla", "What's client say"); ?></h3>
 		        <div class="col-md-6 padding-top-15">
 		        	<i> Etiam scelerisque a turpis ac rhoncus. Fusce congue nibh at lorem ultrices, vel semper odio bibendum.</i>
 		        	<h3 class="text-white pull-right">John Doe</h3>
@@ -96,7 +96,7 @@
 	</div>
 	<div id="ccouer-home" class="col-md-12 no-padding" style="background-image:url('<?php echo Yii::app()->theme->baseUrl; ?>/assets/img/reunion/reunion3.jpg');">
 		<h1 class="col-xs-12 letter-orange-2 text-center text-white text-lightweight no-margin padding-top-50">
-			Nos coups de coeurs
+			<?php echo Yii::t("terla", "Nos coups de coeurs"); ?>
 		</h1><br>
 		<h5 class="col-xs-12 text-white text-center text-lightweight  padding-bottom-50">
 			Proin ultricies viverra enim, vel cursus nulla condimentum a. Etiam nibh ligula, molestie sit amet
@@ -109,9 +109,9 @@
 		         		alt="Generic placeholder thumbnail" class="img-responsive">
 		      </a>
 		      <div class="col-md-12 content-desc">
-		      <h4 class="letter-lightgray">Title</h4>
+		      <h4 class="letter-lightgray"><?php echo Yii::t("terla", "Title"); ?></h4>
 		      <p>Ut dictum elit a magna tincidunt ornare. Praesent volutpat amet diam non iaculis. Vestibulum id velit pulvinar, aliquam fermentum neque.</p>
-		      <a href="#" class="btn bg-orange btn-sm bold letter-white">See +</a>
+		      <a href="#" class="btn bg-orange btn-sm bold letter-white"><?php echo Yii::t("terla", "See +"); ?></a>
 		      </div>
 		   	</div>
 		   </div>
@@ -123,9 +123,9 @@
 		         		alt="Generic placeholder thumbnail" class="img-responsive">
 		      </a>
 		      <div class="col-md-12 content-desc">
-		      <h4 class="letter-lightgray">Title</h4>
+		      <h4 class="letter-lightgray"><?php echo Yii::t("terla", "Title"); ?></h4>
 		      <p>Ut dictum elit a magna tincidunt ornare. Praesent volutpat amet diam non iaculis. Vestibulum id velit pulvinar, aliquam fermentum neque.</p>
-		      <a href="#" class="btn bg-orange btn-sm bold letter-white">See +</a>
+		      <a href="#" class="btn bg-orange btn-sm bold letter-white"><?php echo Yii::t("terla", "See +"); ?></a>
 		      </div>
 		   	</div>
 		   </div>
@@ -136,9 +136,9 @@
 		         		alt="Generic placeholder thumbnail" class="img-responsive">
 		      </a>
 		      <div class="col-md-12 content-desc">
-		      <h4 class="letter-lightgray">Title</h4>
+		      <h4 class="letter-lightgray"><?php echo Yii::t("terla", "Title"); ?></h4>
 		      <p>Ut dictum elit a magna tincidunt ornare. Praesent volutpat amet diam non iaculis. Vestibulum id velit pulvinar, aliquam fermentum neque.</p>
-		      <a href="#" class="btn bg-orange btn-sm bold letter-white">See +</a>
+		      <a href="#" class="btn bg-orange btn-sm bold letter-white"><?php echo Yii::t("terla", "See +"); ?></a>
 		      </div>
 		   	</div>
 		   </div>
@@ -149,9 +149,9 @@
 		         		alt="Generic placeholder thumbnail" class="img-responsive">
 		      </a>
 		      <div class="col-xs-12 content-desc">
-		      <h4 class="letter-lightgray">Title</h4>
+		      <h4 class="letter-lightgray"><?php echo Yii::t("terla", "Title"); ?></h4>
 		      <p>Ut dictum elit a magna tincidunt ornare. Praesent volutpat amet diam non iaculis. Vestibulum id velit pulvinar, aliquam fermentum neque.</p>
-		      <a href="#" class="btn bg-orange btn-sm bold letter-white">See +</a>
+		      <a href="#" class="btn bg-orange btn-sm bold letter-white"><?php echo Yii::t("terla", "See +"); ?></a>
 		      </div>
 		   	</div>
 		   </div>
@@ -160,8 +160,9 @@
 	</div>
 	<div class="col-xs-12 padding-20 bg-orange text-center">
 		<h4 class="text-white text-lightweight">
-			Envie d'encore plus ? Aller sur notre carte pour composer votre propres circuits
-			<button class="btn btn-orange-border-white bg-orange text-white btn-show-map margin-left-15">Découvrir</button>
+			<?php echo Yii::t("terla", "Envie d'encore plus ? Aller sur notre carte pour composer votre propres circuits"); ?>
+			
+			<button class="btn btn-orange-border-white bg-orange text-white btn-show-map margin-left-15"><?php echo Yii::t("terla", "Discover"); ?></button>
 		</h4>
 	</div>
 	
@@ -176,7 +177,7 @@
 				         		class="img-circle" height=50
 				         		alt="Generic placeholder thumbnail">
 				      </a> 
-				      <label class="margin-10 text-lightweight">Type de prestation</label>
+				      <label class="margin-10 text-lightweight"><?php echo Yii::t("terla", "Kind of prestation"); ?></label>
 				   </div>
 				<?php } ?>   
 			</div>
@@ -185,7 +186,10 @@
 
 		<h3 class="col-xs-12 text-center margin-top-50"> 
 			<i class="fa fa-envelope fa-3x letter-red"></i><br>
-			<small class="text-black">INSCRIVEZ VOUS À NOTRE<br>NEWSLETTER</small>
+			<small class="text-black">
+				<?php echo Yii::t("terla", "INSCRIVEZ VOUS À NOTRE"); ?>
+				<br><?php echo Yii::t("terla", "NEWSLETTER"); ?>
+			</small>
 		</h3>
 
 		<br>
@@ -194,7 +198,7 @@
 
 		<div class="col-sm-6 col-sm-offset-3 margin-bottom-50">
 			<input class="col-xs-9 input-newsletter" type="text">
-			<button class="btn btn-link col-xs-3 bg-orange letter-white btn-newsletter">Inscription</button>
+			<button class="btn btn-link col-xs-3 bg-orange letter-white btn-newsletter"><?php echo Yii::t("terla", "Subscribe"); ?></button>
 		</div>
 
 	</div>
