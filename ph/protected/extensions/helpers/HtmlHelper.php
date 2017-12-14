13bab8862ecb9650be2a05d24ef9965fdb2a3b3e
codev <raphaelriviere06@gmail.com>
2017-12-14 15:26:37 +0400

    hotfix sur le chargement des fichier overwrite

--- ph/protected/extensions/helpers/HtmlHelper.php
+++ ph/protected/extensions/helpers/HtmlHelper.php
@@ -31,59 +31,86 @@ class HtmlHelper {
         if($path == null)
             $path = Yii::app()->request->baseUrl;
         $ajaxRequest = Yii::app()->request->isAjaxRequest;
+        if(!empty(Yii::app()->params["overWrite"]))
+            $pathOverwrite = Yii::app()->getModule(Yii::app()->theme->name)->getAssetsUrl()."/";
+
         foreach ($files as $file) {
             $extention = pathinfo($file,PATHINFO_EXTENSION);
             if ($extention == "js" || $extention == "JS") {
                 $fileVersion = $file."?v=".Yii::app()->params["version"] ;
-                if($ajaxRequest){
-                    echo CHtml::scriptFile($path.$fileVersion);
-                } else {
-                    $cs->registerScriptFile($path. $fileVersion , CClientScript::POS_END, array(), 2);
+                self::scriptFile($ajaxRequest, $path.$fileVersion);
+                if( !empty($pathOverwrite) && in_array(substr($file, 1), Yii::app()->params["overWrite"]["assets"])  ) {
+                    $file = substr($file, 1)."?v=".Yii::app()->params["version"] ;
+                    self::scriptFile($ajaxRequest, $pathOverwrite.$file);
                 }
+
             } else if ($extention == "css" || $extention == "CSS") {
-                if($ajaxRequest){
-                    echo CHtml::cssFile($path.$file);
-                } else {
-                    $cs->registerCssFile($path.$file);
+                self::cssFile($ajaxRequest, $path.$file);
+                if( !empty($pathOverwrite) && in_array(substr($file, 1), Yii::app()->params["overWrite"]["assets"])  ) {
+                    $file = substr($file, 1) ;
+                    self::cssFile($ajaxRequest, $pathOverwrite.$file);
                 }
             } else {
                 //unknown extension
                 throw new InvalidArgumentException("unkonw file extension : ".$extention);
             }
         }
+        
         return true;
+       
     }
 
-    public static function registerCssAndScriptsFilesOverwrite() {
-        if(!empty(Yii::app()->params["overWrite"])){
-            $cs = Yii::app()->getClientScript();
-            $path = Yii::app()->getModule('terla')->getAssetsUrl()."/";
-            $ajaxRequest = Yii::app()->request->isAjaxRequest;
-            foreach (Yii::app()->params["overWrite"]["assets"] as $key => $file) {
-                $extention = pathinfo($file,PATHINFO_EXTENSION);
-                if ($extention == "js" || $extention == "JS") {
-                    $file = $file."?v=".Yii::app()->params["version"] ;
-                    if($ajaxRequest){
-                        echo CHtml::scriptFile($path.$file);
-                    } else {
-                        $cs->registerScriptFile($path. $file , CClientScript::POS_END, array(), 2);
-                    }
-                }
-                else if ($extention == "css" || $extention == "CSS") {
-                    if(file_exists ( $path.$file ) ) {
-                        if($ajaxRequest){
-                            echo CHtml::cssFile($path.$file);
-                        } else {
-                            $cs->registerCssFile($path.$file);
-                        }
-                    }
-                } else {
-                    //unknown extension
-                    throw new InvalidArgumentException("unkonw file extension : ".$extention);
-                }
-            } 
+
+    public static function scriptFile($ajaxRequest, $path) {
+        $cs = Yii::app()->getClientScript();
+         if($ajaxRequest){
+            echo CHtml::scriptFile($path);
+        } else {
+            $cs->registerScriptFile($path, CClientScript::POS_END, array(), 2);
         }
-        return true;
     }
+
+    public static function cssFile($ajaxRequest, $path) {
+        $cs = Yii::app()->getClientScript();
+        if($ajaxRequest){
+            echo CHtml::cssFile($path);
+        } else {
+            $cs->registerCssFile($path);
+        }
+    }
+   
+
+    // public static function registerCssAndScriptsFilesOverwrite() {
+    //     if(!empty(Yii::app()->params["overWrite"])){
+    //         $cs = Yii::app()->getClientScript();
+    //         $path = Yii::app()->getModule('terla')->getAssetsUrl()."/";
+    //         $ajaxRequest = Yii::app()->request->isAjaxRequest;
+    //         foreach (Yii::app()->params["overWrite"]["assets"] as $key => $file) {
+                
+    //             $extention = pathinfo($file,PATHINFO_EXTENSION);
+    //             if ($extention == "js" || $extention == "JS") {
+    //                 $file = $file."?v=".Yii::app()->params["version"] ;
+    //                 if($ajaxRequest){
+    //                     echo CHtml::scriptFile($path.$file);
+    //                 } else {
+    //                     $cs->registerScriptFile($path. $file , CClientScript::POS_END, array(), 2);
+    //                 }
+    //             }
+    //             else if ($extention == "css" || $extention == "CSS") {
+    //                 if(file_exists ( $path.$file ) ) {
+    //                     if($ajaxRequest){
+    //                         echo CHtml::cssFile($path.$file);
+    //                     } else {
+    //                         $cs->registerCssFile($path.$file);
+    //                     }
+    //                 }
+    //             } else {
+    //                 throw new InvalidArgumentException("unkonw file extension : ".$extention);
+    //             }
+    //         } 
+    //     }
+    //     //exit;
+    //     return true;
+    // }
 }
 ?>
