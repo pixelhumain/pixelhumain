09ed08e384967f6f053388973f33dd441c488606
Oceatoon <oceatoon@gmail.com>
2018-02-19 09:58:57 +0400

    traduction des tags

---
+++ ph/protected/messages/fr/tags.php
@@ -0,0 +1,31 @@
+<?php 
+return array(
+	"agriculture" => "agriculture",
+	"food" => "alimentation",
+	"nutrition" => "nourriture",
+	"AMAP" => "AMAP",
+	"health" => "santé",
+	"waste" => "déchets",
+	"urbanism" => "aménagement",
+	"transport" => "transport",
+	"construction" => "construction",
+	"education" => "éducation",
+	"childhood" => "petite enfance",
+	"citizen" => "citoyenneté",
+	"ess" => "ess",
+	"social solidarity economy" => "economie social solidaire",
+	"energy" => "énergie",
+	"climat" => "climat",
+	"culture" => "culture",
+	"animation" => "animation",
+	"environment" => "environnement",
+	"biodiversity" => "biodiversité",
+	"ecology" => "écologie",
+	"numeric" => "numérique",
+	"ict" => "tic",
+	"computer" => "informatique",
+	"internet" => "internet",
+	"web" => "web",
+	"sport" => "sport"
+);
+?>
