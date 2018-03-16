4987bb5af1617300e4ead73ba84b551751e3e85a
codev <raphaelriviere06@gmail.com>
2018-03-16 09:26:55 +0400

    Hotfix

--- ph/themes/CO2/views/layouts/initJs.php
+++ ph/themes/CO2/views/layouts/initJs.php
@@ -62,7 +62,7 @@
         type:"<?php echo Organization::COLLECTION ?>"
     };
 
-    var directoryViewMode="<?php echo (@$preferences["directoryView"]) ? $preferences["directoryView"] : "list" ?>";
+    var directoryViewMode="<?php echo "block" ?>";
     //var classifiedSubTypes = <?php //echo json_encode( Classified::$classifiedSubTypes ) ?>;
     var urlTypes = <?php asort(Element::$urlTypes); echo json_encode(Element::$urlTypes) ?>;
     
