1faeb1df0702f8dc4c7973308d6a4d3469d5c073
codev <raphaelriviere06@gmail.com>
2018-03-22 17:18:53 +0400

    New trad invite

---
+++ ph/protected/messages/fr/invite.php
@@ -0,0 +1,9 @@
+<?php
+
+	return array(
+		"Search or invite your contacts" => "Rechercher ou inviter vos contacts",
+		"Search or invite members" => "Rechercher ou inviter des membres",
+		"Import a file" => "Importer un fichier"
+	);
+
+?>
