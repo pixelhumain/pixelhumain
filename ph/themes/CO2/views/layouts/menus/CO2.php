7521a446525b058699d86e6a54a6573714173521
Kgneo <tristan.goguet@gmail.com>
2018-04-06 18:37:39 +1100

    fix bug modal delete amendement

--- ph/themes/CO2/views/layouts/menus/CO2.php
+++ ph/themes/CO2/views/layouts/menus/CO2.php
@@ -542,47 +542,8 @@
     <!-- /.container-fluid -->
 
 </nav>
-<?php //if($subdomain != "welcome"){ 
-        $this->renderPartial($layoutPath.'loginRegister', array("subdomain" => $subdomain)); 
-      //} else{ ?>
-        <!--<div class="modal fade" role="dialog" id="modalRegisterSuccess">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header bg-green-k text-white">
-                        <h4 class="modal-title"><i class="fa fa-check"></i> <?php echo Yii::t("login","Account Created!!") ?></h4>
-                    </div>
-                    <div class="modal-body center text-dark hidden" id="modalRegisterSuccessContent"></div>
-                    <div class="modal-body center text-dark">
-                        
-                        <h4 class="letter-green no-margin"><i class="fa fa-check-circle"></i> Confirmez votre adresse e-mail</h4>
-                        <h4 class="no-margin">
-                            <small>afin d'accéder à votre compte</small>
-                        </h4>
-                        <small class="no-margin">
-                            <i class="fa fa-lock"></i> Pour des raisons de sécurité, vous devez confirmer votre adresse e-mail avant de pouvoir vous connecter.
-                        </small>
-                        <br><br>
-                        <h5><i class="fa fa-angle-down"></i> Comment faire ?</h5>
-                        <i class="fa fa-envelope-open" style="width:20px;"></i> <b>Vérifiez votre boîte e-mails</b><br>
-                        <i class="fa fa-hand-o-up" style="width:20px;"></i> <b>Cliquez sur le lien d'activation</b> que nous vous avons envoyé.</br>
-                        <hr>
-                        <i class="fa fa-unlock" style="width:20px;"></i> Vous serez <b class="letter-green">connecté automatiquement</b> et redirigé vers votre page perso.
-                            
-                    </div>
-                    <div class="modal-footer">
-                         <button type="button" class="btn btn-default letter-green" data-dismiss="modal"><i class="fa fa-check"></i> J'ai compris</button>
-                    </div>
-                </div>
-            </div>
-        </div>-->
-      <?php //}
-?>
-
-<?php /*if(isset(Yii::app()->session['userId'])) {
-        $CO2DomainName = isset(Yii::app()->params["CO2DomainName"]) ? Yii::app()->params["CO2DomainName"] : "CO2";
-        $this->renderPartial($layoutPath.'modals.'.$CO2DomainName.'.selectCreate',  array( ) ); 
-     }*/
-?>
+<?php   $this->renderPartial($layoutPath.'loginRegister', array("subdomain" => $subdomain)); 
+       ?>
 
 <?php if(isset(Yii::app()->session['userId'])) 
         $this->renderPartial($layoutPath.'notifications'); ?>
