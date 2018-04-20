20fd7f985a461e1788e8b5e8e0f3d43725b0240c
ClementPouillaude <38495407+ClementPouillaude@users.noreply.github.com>
2018-04-20 12:36:53 +0200

    Up KCFinderWidget.php
    Up KCFinderWidget.php

--- ph/protected/messages/es/KCFinderWidget.php
+++ ph/protected/messages/es/KCFinderWidget.php
@@ -1,7 +1,7 @@
 <?php 
 return array(
-                'You need to set a upload directory and URL.<br/>Check the documentation.'=>'Vous devez paramétrer le répertoire et lURL dupload. <br/>RApportez vous à la documentation',
-            	'Set the user Id to use this type of storage' => "Alimenter lidentifiant de lutilisateur pour gérer ce type de stockage",
-            	'Set the CKEditor container to initialize this widget' => 'Le container du CKEditor nest pas alimenté',
+                'You need to set a upload directory and URL.<br/>Check the documentation.'=>'Se necesita configurar el repertorio y el URL dupload. <br/>Ver la documentación',
+            	'Set the user Id to use this type of storage' => "Configurar el nombre de usuario para administrar este tipo de almacenamiento",
+            	'Set the CKEditor container to initialize this widget' => 'Configurar el CKEditor para empezar este herramiento',
             );
 ?>
