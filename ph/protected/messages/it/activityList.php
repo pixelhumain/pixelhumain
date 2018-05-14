c3d353b965ea147ca5969784e152000a7e02bef4
Stephan Schildberg <scoid@users.noreply.github.com>
2018-05-14 22:29:02 +0200

    Update activityList.php

--- ph/protected/messages/it/activityList.php
+++ ph/protected/messages/it/activityList.php
@@ -1,8 +1,6 @@
 <?php
-
 	return array(
-		"There is no activity for the moment.<br/>Edit...<br/>To improve the commons informations<br/>To give a better overview of territories' actors and activities<br/>And to liberate your knowledge" => "Il n'y a pas d'activités pour le moment.<br/>Editer...<br/>Pour améliorer l'information collective,<br/>Pour donner un meilleur aperçu des acteurs et de l'activités du territoire<br/>Et pour libérer votre savoir !",
-		"See modifications" => "Voir les modifications"
+		"There is no activity for the moment.<br/>Edit...<br/>To improve the commons informations<br/>To give a better overview of territories' actors and activities<br/>And to liberate your knowledge" => "Attualmente non c'è nessuna attività.<br/>Modifica..<br/>Per migliorare il contenuto dei commons,<br/>Per ottenere una migliore panoramica degli attori e delle attività nella regione<br/>E per condividere le tue conoscenze!",
+		"See modifications" => "Vedi modifiche"
 	)
-
 ?>
