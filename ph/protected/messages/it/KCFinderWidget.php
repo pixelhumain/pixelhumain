6ece690d88b87efdef14cff6cf5f37f16a12d97f
Stephan Schildberg <scoid@users.noreply.github.com>
2018-05-14 22:27:50 +0200

    Update KCFinderWidget.php

--- ph/protected/messages/it/KCFinderWidget.php
+++ ph/protected/messages/it/KCFinderWidget.php
@@ -1,7 +1,7 @@
 <?php 
 return array(
-                'You need to set a upload directory and URL.<br/>Check the documentation.'=>'Vous devez paramétrer le répertoire et lURL dupload. <br/>RApportez vous à la documentation',
-            	'Set the user Id to use this type of storage' => "Alimenter lidentifiant de lutilisateur pour gérer ce type de stockage",
-            	'Set the CKEditor container to initialize this widget' => 'Le container du CKEditor nest pas alimenté',
+                'You need to set a upload directory and URL.<br/>Check the documentation.'=>'È necessario impostare una directory di upload e un URL. <br/>Avvisare la documentazione',
+            	'Set the user Id to use this type of storage' => 'Utilizzare l'ID utente per l'utilizzo di questo tipo di memoria',
+            	'Set the CKEditor container to initialize this widget' => 'Includere CKEditor per inizializzare il widget',
             );
 ?>
