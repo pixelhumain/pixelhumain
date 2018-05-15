89f753a6b49626eab9455bce6ed8a8ec18e15192
Stephan Schildberg <scoid@users.noreply.github.com>
2018-05-15 07:01:03 +0200

    Update classified.php

--- ph/protected/messages/it/classified.php
+++ ph/protected/messages/it/classified.php
@@ -2,10 +2,10 @@
 <?php
 
 	return array(
-		"Send a private message to the advertiser" => "Envoyer un message privé à l'annonceur",
-		"classified published by" => "Annonce publiée par",
-		"Add a classified" => "Publier une annonce",
-		"Publish" => "Publier"
+		"Send a private message to the advertiser" => "Invia un messaggio privato all'inserzionista",
+		"classified published by" => "Annuncio pubblicato da",
+		"Add a classified" => "Pubblica un annuncio",
+		"Publish" => "Pubblica"
 	)
 
 ?>
