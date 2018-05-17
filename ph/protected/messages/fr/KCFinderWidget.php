ee17657a88b156c48f5aeb45d4e814a8c92defb1
dagobertstyle <coucou@mailoo.org>
2018-05-17 18:08:51 +0200

    Update KCFinderWidget.php
    Corrections orthographe, grammaire.
    La dernière phrase ne m'a pas l'air très juste, mais comme je ne sais pas de quoi il s'agit, j'ai laissé tel quel.

--- ph/protected/messages/fr/KCFinderWidget.php
+++ ph/protected/messages/fr/KCFinderWidget.php
@@ -1,7 +1,7 @@
 <?php 
 return array(
-                'You need to set a upload directory and URL.<br/>Check the documentation.'=>'Vous devez paramétrer le répertoire et lURL dupload. <br/>RApportez vous à la documentation',
-            	'Set the user Id to use this type of storage' => "Alimenter lidentifiant de lutilisateur pour gérer ce type de stockage",
-            	'Set the CKEditor container to initialize this widget' => 'Le container du CKEditor nest pas alimenté',
+                'You need to set a upload directory and URL.<br/>Check the documentation.'=>'Vous devez paramétrer le répertoire et l\'URL d\'upload. <br/>Rapportez-vous à la documentation',
+            	'Set the user Id to use this type of storage' => "Alimentez l\'identifiant de l\'utilisateur pour gérer ce type de stockage",
+            	'Set the CKEditor container to initialize this widget' => 'Le container du CKEditor n\'est pas alimenté',
             );
 ?>
