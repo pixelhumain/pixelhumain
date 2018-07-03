5d60517beea63c5e61e47b2510b7bba6aa02a654
Bouboule <clement.damiens@gmail.com>
2018-07-03 08:51:45 +0200

    domTarget upload obj & hotfix afterAll upload method for survey and co2

--- ph/themes/CO2/views/layouts/mainSearch.php
+++ ph/themes/CO2/views/layouts/mainSearch.php
@@ -205,7 +205,11 @@
                 '/plugins/font-awesome/css/font-awesome.min.css',
                 //'/plugins/font-awesome-custom/css/font-awesome.css',
 
-                '/plugins/cryptoJS-v3.1.2/rollups/aes.js'
+                '/plugins/cryptoJS-v3.1.2/rollups/aes.js',
+                //FineUplaoder (called in jquery.dynform.js)
+                '/plugins/fine-uploader/jquery.fine-uploader/fine-uploader-gallery.css',
+                '/plugins/fine-uploader/jquery.fine-uploader/jquery.fine-uploader.js',
+                '/plugins/fine-uploader/jquery.fine-uploader/fine-uploader-new.min.css'
             );
             if(Yii::app()->language!="en")
                 array_push($cssAnsScriptFilesModule,"/plugins/jquery-validation/localization/messages_".Yii::app()->language.".js");
