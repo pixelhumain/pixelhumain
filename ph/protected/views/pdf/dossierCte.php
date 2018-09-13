09f1a9f60cc0c336c0148b4c09bce92b0f19c4c2
codev <raphaelriviere06@gmail.com>
2018-09-13 22:57:34 +0400

    Ajout de parseDown

--- ph/protected/views/pdf/dossierCte.php
+++ ph/protected/views/pdf/dossierCte.php
@@ -1,123 +1,139 @@
-<h1> Dossier N° : <?php echo (string)@$adminAnswers["_id"] ?></h1>
+<?php
+
+Yii::import("parsedown.Parsedown", true);
+
+
+?>
+<style type="text/css">
+	
+h1 {
+	font-size: 16px;
+	
+}
+
+</style>
+
+<span style="text-align: center;">
+	<img src="http://www.tco.re/wp-content/uploads/2018/07/slide-ppt_contrats-de-transition-ecologique-768x74.jpg">
+	<h1> CONTRAT DE TRANSITION ECOLOGIQUE </h1>
+	<span>FICHE ACTION :</span><br/>
+	<span><?php 
+		if( @$answers["cte2"]["answers"]["project"]  ) { 
+			echo $answers["cte2"]["answers"]["project"]["name"];
+		} ?>
+	</span>
+
+</span>
+
 
 <div class='col-xs-12'>
-	<h4 class="padding-20" style=""> Réponse par :</h4>
-	<span> Name : <?php echo $user["name"]; ?> </span> <br/>
-	<span> Email : <?php echo @$answers["cte1"]["answers"]["email"]; ?> </span><br/>
-	<span> Organisations :
-		<?php if( @$answers["cte1"]["answers"]["organization"]  ){ ?>
-			<a href="<?php echo Yii::app()->createUrl( "#page.type.organizations.id.".$answers["cte1"]["answers"]["organization"]["id"]); ?>" target="_blank"><?php echo $answers["cte1"]["answers"]["organization"]["name"]; ?></a>
-		<?php } ?>
-	</span><br/>
-	<span> Projet :
-		<?php if( @$answers["cte2"]["answers"]["project"]  ) { ?>
-			<a href="<?php echo Yii::app()->createUrl( "#page.type.projects.id.".$answers["cte2"]["answers"]["project"]["id"]); ?>" target="_blank"><?php echo $answers["cte2"]["answers"]["project"]["name"]; ?></a>
-		<?php } ?>
-	</span><br/>		
-</div>
+	<h4 class="padding-20" style="">Orientation stratégique</h4>
+	<span> <?php
+		if(!empty($answers["cte3"]["answers"]["objectif"]["usagers"])){
+			echo $answers["cte3"]["answers"]["objectif"]["usagers"];
+		}else
+			echo "<i> Pas renseigner </i>";
+	?> </span> 
+	<br/>
 
-<div class='col-xs-12'>
-	<?php
-		foreach ($form["scenario"] as $k => $v) {
-			echo '<h4 class="padding-20" style="">'.$v["title"].'</h4>';
-			if(@$answers[$k]){
-				//echo '<span class="text-dark">'.date("d/m/Y h:i", $answers[$k]["created"]).'</span>';
-				?>
-				<div class='col-xs-12'>
-					<?php 
-						foreach ( $answers[$k]["answers"] as $key => $value) {
-							$editBtn = "";
-							
-							echo "<div class='col-xs-12'>";
-							echo 	"<h5> [ étape ] ".@$v["form"]["scenario"][$key]["title"]."</h5>";
+	<h4 class="padding-20" style="">Objectifs</h4>
+	<span> <?php
+		if(!empty($answers["cte3"]["answers"]["objectif"]["objectif"])){
+			echo $answers["cte3"]["answers"]["objectif"]["objectif"];
+		}else
+			echo "<i> Pas renseigner </i>";
+	?> </span> 
+	<br/>
 
-							if( @$v["form"]["scenario"][$key]["json"] ){
-								$formQ = @$v["form"]["scenario"][$key]["json"]["jsonSchema"]["properties"];
-								foreach ($value as $q => $a){
-									if(is_string($a)){
-										echo '<span><i>'.@$formQ[ $q ]["placeholder"]."</i></span><br/>";
-										$markdown = (strpos(@$formQ[ $q ]["class"], 'markdown') !== false) ? 'markdown' : "";
-										echo "<span style='' class='".$markdown."'>".$a."</span><br/>";
-									}else if(@$a["type"] && $a["type"]==Document::COLLECTION){
-										$document=Document::getById($a["id"]);
-										$document["docId"]=$a["id"];
-										$answers[$k]["answers"][$key]["files"]=array($document);
-										$path=Yii::app()->getRequest()->getBaseUrl(true)."/upload/communecter/".$document["folder"]."/".$document["name"];
-										echo '<span><i>'.@$formQ[ $q ]["placeholder"]."</i></span><br/>";
-										echo "<span>";
-											echo "<a href='".$path."' target='_blank'><i class='fa fa-file-pdf-o text-red'></i> ".$document["name"]."</a>";
-										echo '</span>';
-									}
-								}
-							//todo search dynamically if key exists
-							}else if(@$v["form"]["scenario"]["survey"]["json"][$key]){
-								$formQ = $v["form"]["scenario"]["survey"]["json"][$key]["jsonSchema"]["properties"];
-								foreach ($value as $q => $a) {
-									if(is_string($a)){
-										echo '<span><i>'.@$formQ[ $q ]["placeholder"]."</i></span><br/>";
-										echo "<span>".$a."</span><br/>";
-									}else if(@$a["type"] && $a["type"]==Document::COLLECTION){
-										echo '<span><i>'.@$formQ[ $q ]["placeholder"]."</i></span><br/>";
-										echo "<span>".$a["type"]."</span><br/>";
-									}
-								}
-							}else if (@$v["form"]["scenario"][$key]["saveElement"]){
-								$el = Element::getByTypeAndId( $value["type"] , $value["id"] );
-								echo '<span><i>'.Yii::t("common","Name")."</i></span><br/>";
-								echo "<span><a target='_blank' class='btn btn-default' href='".Yii::app()->createUrl("#@".$el["slug"]).".view.detail'>".$el["name"]."</a>";
-								echo "</span><br/>";
-								if(@$el["type"]){
-									echo '<span><i>'.Yii::t("common","Type")."</i></span><br/>";
-									echo "<span>".$el["type"]."</span><br/>";
-								}
-								if(@$el["description"]){
-									echo '<span><i>'.Yii::t("common", "Description")."</i></span><br/>";
-									echo "<span>".$el["description"]."</span><br/>";
-								}
-								if(@$el["tags"]){
-									echo '<span><i>'.Yii::t("common","Tags")."</i></span><br/>";
-									echo "<span>".
-										$it=0;
-										foreach($el["tags"] as $tags){
-											if($it>0)
-												echo ", ";
-											echo "<span class='text-red'>#".$tags."</span>";
-											$it++;
-										}
-										echo "</span><br/>";
-								}
-								if(@$el["shortDescription"]){
-									echo '<span><i>'.Yii::t("common","Short description")."</i></span><br/>";
-									echo "<span>".$el["shortDescription"]."</span><br/>";
-								}
-								if(@$el["email"]){
-									echo '<span><i>'.Yii::t("common","Email")."</i></span><br/>";
-									echo "<span>".$el["email"]."</span><br/>";
-								}
-								
-								if(@$el["profilImageUrl"]){
-									echo '<span><i>'.Yii::t("common","Profil image")."</i></span><br/>";
-									echo "<span><img src='".Yii::app()->createUrl($el["profilImageUrl"])."' class='img-responsive'/>";
-									echo "</span><br/>";
-								}
-								if(@$el["url"]){
-									echo '<span><i>'.Yii::t("common","Website URL")."</i></span><br/>";
-									echo "<span><a href='".$el["url"]."'>".$el["url"]."</a></span><br/>";
-								}
-							}
+	<h4 class="padding-20" style="">Description de l'action</h4>
+	<span> <?php
+		$Parsedown = new Parsedown();
+		if(!empty($answers["cte3"]["answers"]["objectif"]["description"])){
+			echo $Parsedown->text($answers["cte3"]["answers"]["objectif"]["description"]);
+		}else
+			echo "<i> Pas renseigner </i>";
+	?> </span> 
+	<br/>
+
+	<h4 class="padding-20" style="">Maitre d'ouvrage/pilote de l'action</h4>
+	<span> <?php
+		if(!empty($answers["cte3"]["answers"]["objectif"]["description"])){
+			echo $answers["cte3"]["answers"]["objectif"]["description"];
+		}else
+			echo "<i> Pas renseigner </i>";
+	?> </span> 
+	<br/>
+
+	<h4 class="padding-20" style="">Partenaires (actés et potentiels)</h4>
+	<span> <?php
+		if(!empty($answers["cte3"]["answers"]["objectif"]["cooperations"])){
+			echo $answers["cte3"]["answers"]["objectif"]["cooperations"];
+		}else
+			echo "<i> Pas renseigner </i>";
+	?> </span> 
+	<br/>
+
+	<h4 class="padding-20" style="">Modalités de mise en oeuvre</h4>
+	<span> <?php
+		if(!empty($answers["cte3"]["answers"]["objectif"]["gouvernances"])){
+			echo $answers["cte3"]["answers"]["objectif"]["gouvernances"];
+		}else
+			echo "<i> Pas renseigner </i>";
+	?> </span> 
+	<br/>
+
+	<h4 class="padding-20" style="">Calendrier</h4>
+	<span> <?php
+	// "maturity" : {
+ //            "state" : "",
+ //            "description" : "",
+ //            "dateConcept" : "",
+ //            "dateCahier" : "",
+ //            "datePrototype" : "",
+ //            "dateDeveloppement" : "",
+ //            "dateTest" : "22/03/2019",
+ //            "dateProduction" : ""
+		if(!empty($answers["cte2"]["answers"]["maturity"])){
+			$m = $answers["cte2"]["answers"]["maturity"];
+			if(!empty($m["state"]))
+				echo "<span>Etat actuel : ".$m["state"]."</span><br/>";
+			if(!empty($m["description"]))
+				echo $m["description"];
+			if(!empty($m["dateConcept"]))
+				echo "<span>Date : ".$m["dateConcept"]."</span><br/>";
+			if(!empty($m["dateCahier"]))
+				echo "<span>Date : ".$m["dateCahier"]."</span><br/>";
+			if(!empty($m["datePrototype"]))
+				echo "<span>Date : ".$m["datePrototype"]."</span><br/>";
+			if(!empty($m["dateConcept"]))
+				echo "<span>Date : ".$m["dateConcept"]."</span><br/>";
+			if(!empty($m["dateDeveloppement"]))
+				echo "<span>Date : ".$m["dateDeveloppement"]."</span><br/>";
+			if(!empty($m["dateTest"]))
+				echo "<span>Date : ".$m["dateTest"]."</span><br/>";
+			if(!empty($m["dateProduction"]))
+				echo "<span>Date : ".$m["dateProduction"]."</span><br/>";
+		}else
+			echo "<i> Pas renseigner </i>";
+	?> </span> 
+	<br/>
+
+	<h4 class="padding-20" style="">Plan de financement</h4>
+	<span> <?php
+		if(isset($answers["cte3"]["answers"]["previsionel"]["previsionel"])){
+			echo "Voir Annexe";
+		}else
+			echo "<i> Pas renseigner </i>";
+	?> </span> 
+	<br/>
 
 
-							echo "</div>";
-
-						} ?>
-
-				</div>
-
-			<?php
-			}else{
-				echo '<span class="padding-20" style="">Pas encore renseigné</h4>';
-			}
-		}
-
-	?>	
+	<h4 class="padding-20" style="">Evaluation</h4>
+	<span> <?php
+		if(isset($answers["cte2"]["answers"]["risk"]["description"])){
+			echo $answers["cte2"]["answers"]["risk"]["description"];
+		}else
+			echo "<i> Pas renseigner </i>";
+	?> </span> 
+	<br/>		
 </div>
