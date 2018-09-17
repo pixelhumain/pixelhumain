b83624086d8ccbdb49c7687a6058a5df52bf16de
Oceatoon <oceatoon@gmail.com>
2018-09-17 13:11:17 +0400

    tweaks after refactor

--- ph/themes/CO2/views/layouts/menus/cte.php
+++ ph/themes/CO2/views/layouts/menus/cte.php
@@ -145,25 +145,28 @@ $session = (!empty($_GET["session"]) ? $_GET["session"] : "1");
             </li>
 
               <?php 
-                $class = "hidden" ;
-                if( empty($me) || empty($me["address"]) || empty($me["address"]["codeInsee"]))
-                    $class = "";
-                $form = PHDB::findOne( Form::COLLECTION , array( "id"=> ($_GET["id"]) ? $_GET["id"] : "cte"));
-             if( Form::canAdmin( (string)$form["_id"] , $form  )) { 
+                
+            $class = "hidden" ;
+            if( empty($me) || empty($me["address"]) || empty($me["address"]["codeInsee"]))
+                $class = "";
+            //$answer = PHDB::findOne( Form::ANSWER_COLLECTION, array("_id"=>new MongoId($_GET["id"])));
+            $form = PHDB::findOne( Form::COLLECTION , array( "id"=> "cte") );
+
+            if( Form::canAdmin( (string)$form["_id"] , $form  ) ) { 
                 ?>
                 <li role="separator" class="divider"></li>
                 <li class="text-admin">
-                    <a href="<?php echo Yii::app()->getRequest()->getBaseUrl(true) ?>/survey/co/answers/id/<?php echo $form["id"]."/session/".$session ?>" class="bg-white">
+                    <a href="<?php echo Yii::app()->getRequest()->getBaseUrl(true) ?>/survey/co/answers/id/cte/session/<?php echo $session ?>" class="bg-white">
                         <i class="fa fa-bars"></i> Projets
                     </a>
                 </li>
                 <li class="text-admin">
-                    <a href="<?php echo Yii::app()->getRequest()->getBaseUrl(true) ?>/survey/co/members/id/<?php echo $form["id"]."/session/".$session ?>" class="bg-white">
+                    <a href="<?php echo Yii::app()->getRequest()->getBaseUrl(true) ?>/survey/co/members/id/cte/session/<?php echo $session ?>" class="bg-white">
                         <i class="fa fa-users"></i> Membres
                     </a>
                 </li>
                 <li class="text-admin">
-                    <a href="<?php echo Yii::app()->getRequest()->getBaseUrl(true) ?>/survey/co/roles/id/<?php echo $form["id"]."/session/".$session ?>" class="bg-white">
+                    <a href="<?php echo Yii::app()->getRequest()->getBaseUrl(true) ?>/survey/co/roles/id/cte/session/<?php echo $session ?>" class="bg-white">
                         <i class="fa fa-file-text"></i> Fiches Actions
                     </a>
                 </li>
