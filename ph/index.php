2c7c314411dd48f7d0df34691651a54dee424f02
codev <raphaelriviere06@gmail.com>
2018-10-30 10:16:30 +0400

    Modif

--- ph/index.php
+++ ph/index.php
@@ -1,5 +1,4 @@
 <?php
-echo "hello worl d"; exit ;
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/vendor/yiisoft/yii/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
