e87bd7d9895a296e99520e3f37c6678014da7362
codev <raphaelriviere06@gmail.com>
2018-10-31 09:26:06 +0400

    Amélioration mail

--- ph/protected/views/emails/notification.php
+++ ph/protected/views/emails/notification.php
@@ -4,14 +4,14 @@ $this->renderPartial('webroot.themes.'.Yii::app()->theme->name.'.views.layouts.m
 ?>
 
 
-<table class="row" style="margin-top : 10px ; border-spacing: 0;border-collapse: collapse;padding: 0;vertical-align: top;text-align: left;width: 100%;position: relative;display: table;">
+<table class="row" style="border-spacing: 0;border-collapse: collapse;padding: 0;vertical-align: top;text-align: left;width: 100%;position: relative;display: table;">
 	<tbody>
 		<tr style="padding: 0;vertical-align: top;text-align: left;"> <!-- Horizontal Digest Content -->
 			<th class="small-12 large-12 columns first" style="color: #3c5665;font-family: Helvetica, Arial, sans-serif;font-weight: normal;padding: 0;margin: 0 auto;text-align: left;line-height: 19px;font-size: 15px;padding-left: 16px;padding-bottom: 16px;width: 564px;padding-right: 8px;">
+
 				<table style="border-spacing: 0;border-collapse: collapse;padding: 0;vertical-align: top;text-align: left;width: 100%;">
 					<tr style="padding: 0;vertical-align: top;text-align: left;">
 						<th style="color: #3c5665;font-family: Helvetica, Arial, sans-serif;font-weight: normal;padding: 0;margin: 0;text-align: left;line-height: 19px;font-size: 15px;">
-							<!--http://localhost:8888/ph/images/betatest.png-->
 							<b>
 								<h5 style="color: inherit;font-family: Helvetica, Arial, sans-serif;font-weight: normal;padding: 0;margin: 0;text-align: center;line-height: 1.3;word-wrap: normal;margin-bottom: 10px;font-size: 20px;">
 									<?php echo Yii::t("mail","Reviens sur COmmunecter, il y a du nouveau") ?>
@@ -20,67 +20,42 @@ $this->renderPartial('webroot.themes.'.Yii::app()->theme->name.'.views.layouts.m
 							<br/>
 							<?php 
 							foreach ($data["data"] as $key => $value) {
-								//var_dump($value["value"] ); exit ;
-								//foreach ($elt as $key => $value) {
+								echo "<div style='padding-left:10px;'><p>".Mail::translateLabel($value)." </p>";
+								if(!empty($value["value"]))
+									echo "<p style='padding:10px 20px;margin:1%;border:1px solid lightgray; font-style:italic; border-radius:10px; width:90%;white-space: pre-line;'>".$value["value"]." </p></br>";
 
-									//if(!empty($value["post"])){
-
-									// $name = $value["name"];
-
-									// if( !empty($value["url"]) ){
-									// 	$color = Element::getColorIcon($value[""]);
-									// 	$name = "<a href='".$value["url"]."' target='_blank' style='color:".$color.";font-weight:800;font-variant:small-caps;'>".$value["name"]."</a>";
-									// }
-
-									// echo "Dans ".$name." : <br/><br/>";
-									//foreach ($value["data"] as $keyD => $valD) {
-										// if(!empty($value["post"])){
-									echo "<div style='padding-left:10px;'><p>".Mail::translateLabel($value)." </p>";
-									if(!empty($value["value"]))
-										echo "<p style='padding:10px 20px;margin:1%;border:1px solid lightgray; font-style:italic; border-radius:10px; width:90%;white-space: pre-line;'>".$value["value"]." </p></br>";
-
-									echo "</div>";
-
-									// if(!empty($value["add"])){
-
-									// 	foreach ($value["add"] as $keyAdd => $valueAdd) {
-									// 		echo '<a href="'.$valueAdd["url"].'" style="color: #728289;font-family: Helvetica, Arial, sans-serif;font-weight: bold;padding: 0;margin: 0;text-align: left;line-height: 1.3;text-decoration: none;">';
-									// 		echo "<p>".Mail::translateLabel($valueAdd)." </p></a><br/>";
-									// 	}
-										
-									// }
-
-								//}
+								echo "</div>";
 							}
+
 							$nbN = $data["countData"] - count($data["data"]) ;
 							if( $nbN > 0 ) 
 								echo "<div style='padding-left:10px;'><p>"."Il vous reste ".$nbN." a découvrire sur Communecter "." </p></div>";
 							?>
 						</th>
-					</tr>
-					<tr style="padding: 0;vertical-align: top;text-align: left;">
-						<th style="color: #3c5665;font-family: Helvetica, Arial, sans-serif;font-weight: normal;padding: 0;margin: 0;text-align: left;line-height: 19px;font-size: 15px;">
-							<br/><br/>
-							<p style="margin: 0;margin-bottom: 10px;color: #3c5665 !important;font-family: Helvetica, Arial, sans-serif;font-weight: normal;padding: 0;text-align: left;line-height: 19px;font-size: 15px;"><?php echo Yii::t("mail","See you soon on") ?> <a href="<?php echo Yii::app()->getRequest()->getBaseUrl(true) ?>" style="color: #728289;font-family: Helvetica, Arial, sans-serif;font-weight: bold;padding: 0;margin: 0;text-align: left;line-height: 1.3;text-decoration: none;"><?php echo Yii::app()->getRequest()->getBaseUrl(true) ?></a></p>
-							<p style="margin: 0;margin-bottom: 10px;color: #3c5665 !important;font-family: Helvetica, Arial, sans-serif;font-weight: normal;padding: 0;text-align: left;line-height: 19px;font-size: 15px;"><?php echo Yii::t("mail","The Human Pixels") ?></p>
-
-							<br/>
-							<p style="margin: 0;margin-bottom: 10px;color: #3c5665 !important;font-family: Helvetica, Arial, sans-serif;font-weight: normal;padding: 0;text-align: left;line-height: 19px;font-size: 12px;">
-							<?php echo Yii::t("mail","Vous pouvez modifier vos préfèrences ici") ; ?>
-							<div style="word-break: break-all;font-size: 12px;"><?php echo Yii::t("mail","si vous souhaité supprimer votre comptes c'est par ici") ; ?></div></p>
-						</th>
-					</tr>
+					</tr>					
 				</table>
+
 	        </th>
 		</tr>
+
+		<tr style="padding: 0;vertical-align: top;text-align: left;">
+			<td style="color: #3c5665;font-family: Helvetica, Arial, sans-serif;font-weight: normal;padding: 0;margin: 0;text-align: left;line-height: 19px;font-size: 15px;">
+				
+				<?php $this->renderPartial('webroot.themes.'.Yii::app()->theme->name.'.views.layouts.mail.footer'); ?>
+
+			</td>
+
+		</tr>
+
 	</tbody>
 </table>
-</td></tr></tbody></table> <!-- End main email content -->
+
+<!-- </td></tr></tbody></table> End main email content -->
     
-<table class="container text-center" style="border-spacing: 0;border-collapse: collapse;padding: 0;vertical-align: top;text-align: center;background: #fefefe;width: 580px;margin: 0 auto;">
+<!-- <table class="container text-center" style="border-spacing: 0;border-collapse: collapse;padding: 0;vertical-align: top;text-align: center;background: #fefefe;width: 580px;margin: 0 auto;">
 	<tbody>
 		<tr style="padding: 0;vertical-align: top;text-align: left;">
-			<td style="word-wrap: break-word;-webkit-hyphens: auto;-moz-hyphens: auto;hyphens: auto;padding: 0;vertical-align: top;text-align: left;color: #3c5665;font-family: Helvetica, Arial, sans-serif;font-weight: normal;margin: 0;line-height: 19px;font-size: 15px;border-collapse: collapse !important;"> <!-- Footer -->
+			<td style="word-wrap: break-word;-webkit-hyphens: auto;-moz-hyphens: auto;hyphens: auto;padding: 0;vertical-align: top;text-align: left;color: #3c5665;font-family: Helvetica, Arial, sans-serif;font-weight: normal;margin: 0;line-height: 19px;font-size: 15px;border-collapse: collapse !important;"> 
 				<table class="row grey" style="border-spacing: 0;border-collapse: collapse;padding: 0;vertical-align: top;text-align: left;background: #f0f0f0;width: 100%;position: relative;display: table;">
 					<tbody>
 						<tr style="padding: 0;vertical-align: top;text-align: left;">
@@ -88,7 +63,7 @@ $this->renderPartial('webroot.themes.'.Yii::app()->theme->name.'.views.layouts.m
 								<table style="border-spacing: 0;border-collapse: collapse;padding: 0;vertical-align: top;text-align: left;width: 100%;">
 									<tr style="padding: 0;vertical-align: top;text-align: left;">
 										<th style="color: #3c5665;font-family: Helvetica, Arial, sans-serif;font-weight: normal;padding: 0;margin: 0;text-align: left;line-height: 19px;font-size: 15px;">
-											<p class="text-center footercopy" style="margin: 0;margin-bottom: 10px;color: #777777 !important;font-family: Helvetica, Arial, sans-serif;font-weight: normal;padding: 20px 0px;text-align: center;line-height: 19px;font-size: 12px;"><?php echo Yii::t("mail","Mail sent from") ?> <?php echo Yii::app()->getRequest()->getBaseUrl(true) ?></p>
+											<p class="text-center footercopy" style="margin: 0;margin-bottom: 10px;color: #777777 !important;font-family: Helvetica, Arial, sans-serif;font-weight: normal;padding: 20px 0px;text-align: center;line-height: 19px;font-size: 12px;"><?php //echo Yii::t("mail","Mail sent from") ?> <?php //echo Yii::app()->getRequest()->getBaseUrl(true) ?></p>
 										</th>
 										<th class="expander" style="color: #3c5665;font-family: Helvetica, Arial, sans-serif;font-weight: normal;padding: 0 !important;margin: 0;text-align: left;line-height: 19px;font-size: 15px;visibility: hidden;width: 0;"></th>
 									</tr>
@@ -100,7 +75,8 @@ $this->renderPartial('webroot.themes.'.Yii::app()->theme->name.'.views.layouts.m
 			</td>
 		</tr>
 	</tbody>
-</table>
+</table> -->
+
 
 
   </center></td></tr></table>
